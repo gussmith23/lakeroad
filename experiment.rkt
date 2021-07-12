b72b506d38ac752aa267da785b0d1177f51d1bb4
Gus Smith <guscomps@gmail.com>
2021-07-11 19:05:16 -0700

    Comment format

--- experiment.rkt
+++ experiment.rkt
@@ -41,11 +41,14 @@
 
 ; lut has input width of 2, what's the value of memory that implements AND
 
-; cool first target: from 1. slice description and 2. highl evel program (e.g. add), find a "clever" (i.e. using a mux) way to implement the program
+; cool first target: from 1. slice description and 2. highl evel program (e.g. add), find a "clever"
+; (i.e. using a mux) way to implement the program
 
-; another optimization: if you have an 8-bit x+1 circuit, xilinx tools can remove the carry chain to make it 1 cycle faster
+; another optimization: if you have an 8-bit x+1 circuit, xilinx tools can remove the carry chain to
+; make it 1 cycle faster
 
-; one way to think about it: software people started with structure, and now are being more free form in their search. hardware people started with no structure and we are now trying to add structure back in
-; alex aiken talk is an example of sw people loosening structure
-; adding structure helps guide the search
-; is this an artifact of old systems being so small? so they were much smaller and the problem was easier
+; one way to think about it: software people started with structure, and now are being more free form
+; in their search. hardware people started with no structure and we are now trying to add structure
+; back in alex aiken talk is an example of sw people loosening structure adding structure helps guide
+; the search is this an artifact of old systems being so small? so they were much smaller and the
+; problem was easier
