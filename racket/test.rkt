722661201e4978f8aef0a1e1779f89524282445c
Gus Smith <guscomps@gmail.com>
2022-01-24 17:53:33 -0800

    Monday work

---
+++ racket/test.rkt
@@ -0,0 +1,67 @@
+#lang racket
+
+(provide main)
+
+(require rosette
+         "ultrascale.rkt")
+
+(define-namespace-anchor anc)
+(define ns (namespace-anchor->namespace anc))
+
+(define (main)
+
+  ; Get file contents.
+  (define file-contents (port->string (current-input-port) #:close? #t))
+
+  (define prog (eval (read (open-input-string file-contents)) ns))
+
+  (define-symbolic* cin (bitvector 1))
+  (define-symbolic* lut-memory-a (bitvector 128))
+  (define-symbolic* lut-memory-b (bitvector 128))
+  (define-symbolic* lut-memory-c (bitvector 128))
+  (define-symbolic* lut-memory-d (bitvector 128))
+  (define-symbolic* lut-memory-e (bitvector 128))
+  (define-symbolic* lut-memory-f (bitvector 128))
+  (define-symbolic* lut-memory-g (bitvector 128))
+  (define-symbolic* lut-memory-h (bitvector 128))
+
+  (define-symbolic* logical-input-0 (bitvector 8))
+  (define-symbolic* logical-input-1 (bitvector 8))
+  (define-symbolic* logical-input-2 (bitvector 8))
+  (define-symbolic* logical-input-3 (bitvector 8))
+  (define-symbolic* logical-input-4 (bitvector 8))
+  (define-symbolic* logical-input-5 (bitvector 8))
+
+  (define out
+    (apply ultrascale-clb
+           cin
+           lut-memory-a
+           lut-memory-b
+           lut-memory-c
+           lut-memory-d
+           lut-memory-e
+           lut-memory-f
+           lut-memory-g
+           lut-memory-h
+           (ultrascale-logical-to-physical-inputs logical-input-0
+                                                  logical-input-1
+                                                  logical-input-2
+                                                  logical-input-3
+                                                  logical-input-4
+                                                  logical-input-5)))
+  (define model
+    (synthesize #:forall (list logical-input-0
+                               logical-input-1
+                               logical-input-2
+                               logical-input-3
+                               logical-input-4
+                               logical-input-5)
+                #:guarantee (begin
+                              (assume (bvzero? logical-input-2))
+                              (assume (bvzero? logical-input-3))
+                              (assume (bvzero? logical-input-4))
+                              (assume (bvzero? logical-input-5))
+                              (assert (bveq (prog logical-input-0 logical-input-1) out)))))
+
+  ; For now just return whether or not we could synthesize anything.
+  (exit (if (unsat? model) 1 0)))
--- rust/src/language.rs
+++ rust/src/language.rs
@@ -1,8 +1,15 @@
+use std::{
+    collections::HashMap,
+    io::Write,
+    process::{Command, Stdio},
+};
+
 use crate::language::LanguageAnalysisData::*;
-use egg::{define_language, Analysis, DidMerge, EGraph, Id};
+use egg::{define_language, Analysis, AstSize, DidMerge, EGraph, Extractor, Id, RecExpr};
+use rayon::prelude::*;
 
 define_language! {
-    enum Language {
+    pub enum Language {
         // (var <name: String> <bitwidth: Num>)
         "var" = Var([Id; 2]),
         // (const <val: Num> <bitwidth: Num>)
@@ -25,9 +32,9 @@ define_language! {
 }
 
 #[derive(Default)]
-struct LanguageAnalysis;
+pub struct LanguageAnalysis;
 #[derive(Debug, Clone, PartialEq)]
-enum LanguageAnalysisData {
+pub enum LanguageAnalysisData {
     // Represents a signal with the given bitwidth.
     Signal(usize),
     _String(String),
@@ -72,6 +79,129 @@ impl Analysis<Language> for LanguageAnalysis {
     }
 }
 
+/// Returns the string representing the Racket expression, and a map mapping
+/// symbol names to their bitwidths.
+pub fn to_racket(expr: &RecExpr<Language>, id: Id) -> (Option<String>, HashMap<String, usize>) {
+    let mut map = HashMap::default();
+    let racket_string = to_racket_helper(expr, id, &mut map);
+    (racket_string, map)
+}
+
+fn to_racket_helper(
+    expr: &RecExpr<Language>,
+    id: Id,
+    map: &mut HashMap<String, usize>,
+) -> Option<String> {
+    match expr[id] {
+        Language::Var([name_id, bw_id]) => match (&expr[name_id], &expr[bw_id]) {
+            (Language::String(v), Language::Num(bw)) => {
+                map.insert(v.clone(), (*bw).try_into().unwrap());
+                Some(v.clone())
+            }
+            _ => panic!(),
+        },
+        Language::Const([val_id, bitwidth_id]) => Some(format!(
+            "(bv {val} {bitwidth})",
+            val = match &expr[val_id] {
+                Language::Num(v) => v.clone(),
+                _ => panic!(),
+            },
+            bitwidth = match expr[bitwidth_id] {
+                Language::Num(v) => v.clone(),
+                _ => panic!(),
+            },
+        )),
+        Language::Add([a_id, b_id])
+        | Language::Sub([a_id, b_id])
+        | Language::Or([a_id, b_id])
+        | Language::And([a_id, b_id])
+        | Language::Xor([a_id, b_id])
+        | Language::AlgebraicShiftRight([a_id, b_id]) => Some(format!(
+            "({op} {a} {b})",
+            op = match expr[id] {
+                Language::Add(_) => "bvadd",
+                Language::Sub(_) => "bvsub",
+                Language::Or(_) => "bvor",
+                Language::And(_) => "bvand",
+                Language::Xor(_) => "bvxor",
+                Language::AlgebraicShiftRight(_) => "bvashr",
+                _ => unreachable!(),
+            },
+            a = to_racket_helper(expr, a_id, map).unwrap(),
+            b = to_racket_helper(expr, b_id, map).unwrap()
+        )),
+        Language::Num(_) => None,
+        Language::String(_) => None,
+    }
+}
+
+pub fn call_racket(expr: String, map: &HashMap<String, usize>) -> bool {
+    let full_expr = format!(
+        "
+    (begin
+        {defines}
+        (define (f {args}) {expr})
+        f)",
+        defines = map
+            .iter()
+            .map(|(k, v)| { format!("(define-symbolic {} (bitvector {}))", k, v) })
+            .collect::<Vec<_>>()
+            .join("\n"),
+        args = map
+            .keys()
+            .into_iter()
+            .cloned()
+            .collect::<Vec<_>>()
+            .join(" "),
+        expr = expr,
+    );
+
+    let mut cmd = Command::new("racket");
+    cmd.arg("-tm");
+    cmd.arg("../racket/test.rkt");
+    cmd.stdin(Stdio::piped());
+    cmd.stdout(Stdio::piped());
+    cmd.stderr(Stdio::piped());
+    let mut proc = cmd.spawn().ok().expect("Failed to spawn process");
+    proc.stdin
+        .as_mut()
+        .unwrap()
+        .write_all(full_expr.as_bytes())
+        .unwrap();
+    let output = proc.wait_with_output().unwrap();
+
+    output.status.success()
+}
+
+pub fn explore(egraph: &EGraph<Language, LanguageAnalysis>, _id: Id) -> HashMap<Id, bool> {
+    let extractor = Extractor::new(egraph, AstSize);
+    let out: HashMap<Id, bool> = egraph
+        .classes()
+        .par_bridge()
+        .map(|eclass| {
+            let (_, expr) = extractor.find_best(eclass.id);
+            let (racket_expr, map) = to_racket(&expr, (expr.as_ref().len() - 1).into());
+            if racket_expr.is_none() {
+                println!("Not attempting to synthesize:\n{}", expr.pretty(80));
+                (eclass.id, false)
+            } else {
+                println!("Attempting to synthesize:\n{}", expr.pretty(80),);
+                let result = call_racket(racket_expr.unwrap(), &map);
+                (eclass.id, result)
+            }
+        })
+        .collect();
+
+    println!("ISA:");
+    for (k, v) in out.iter() {
+        if *v {
+            println!("{}", extractor.find_best(*k).1.pretty(80))
+        }
+    }
+
+    out
+}
+
 #[cfg(test)]
 mod tests {
     use std::str::FromStr;
@@ -85,13 +215,54 @@ mod tests {
         let mut egraph: EGraph<Language, LanguageAnalysis> = EGraph::default();
 
         let id = egraph.add_expr(
-            &RecExpr::from_str("(- (| (var x 8) (var y 8)) (asr (^ (var x 8) (var y 8)) (const 1 8)))")
-                .unwrap(),
+            &RecExpr::from_str(
+                "(- (| (var x 8) (var y 8)) (asr (^ (var x 8) (var y 8)) (const 1 8)))",
+            )
+            .unwrap(),
         );
 
         match &egraph[id].data {
             Signal(8) => (),
             _ => panic!(),
         }
+    }
+
+    #[test]
+    fn ceil_avg_to_racket() {
+        let expr = &RecExpr::from_str(
+            "(- (| (var x 8) (var y 8)) (asr (^ (var x 8) (var y 8)) (const 1 8)))",
+        )
+        .unwrap();
+
+        assert_eq!(
+            to_racket(expr, (expr.as_ref().len() - 1).into()).0.unwrap(),
+            ""
+        );
+    }
+
+    #[test]
+    fn ceil_avg_to_racket_call_racket() {
+        let expr = &RecExpr::from_str(
+            "(- (| (var x 8) (var y 8)) (asr (^ (var x 8) (var y 8)) (const 1 8)))",
+        )
+        .unwrap();
+
+        let (expr, map) = to_racket(expr, (expr.as_ref().len() - 1).into());
+
+        assert!(!call_racket(expr.unwrap(), &map));
+    }
+
+    #[test]
+    fn ceil_avg_explore() {
+        let mut egraph: EGraph<Language, LanguageAnalysis> = EGraph::default();
+
+        let id = egraph.add_expr(
+            &RecExpr::from_str(
+                "(- (| (var x 8) (var y 8)) (asr (^ (var x 8) (var y 8)) (const 1 8)))",
+            )
+            .unwrap(),
+        );
+
+        explore(&egraph, id);
     }
 }
