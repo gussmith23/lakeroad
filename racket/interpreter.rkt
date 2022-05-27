42a69db372a939b779b2636e51f42fad49c24953
Gus Smith <guscomps@gmail.com>
2022-05-27 09:02:56 -0700

    Add physical-to-logical to interpreter

--- racket/interpreter.rkt
+++ racket/interpreter.rkt
@@ -8,6 +8,7 @@
 (define (interpret expr)
   (match expr
     [`(logical-to-physical-inputs ,_ ...) (interpret-logical-to-physical-inputs interpret expr)]
+    [`(physical-to-logical-mapping ,_ ...) (interpret-physical-to-logical-mapping interpret expr)]
     ;;; Everything else gets returned as-is. This means that calling (interpret) on Racket expressions
     ;;; gives back Racket expressions, meaning we can use Racket expressions in our DSL.
     [other other]))
@@ -20,6 +21,9 @@
   (check-equal? (interpret 1) 1)
   (check-equal? (interpret (list 1 2 3)) (list 1 2 3))
   (check-equal? (interpret 'test) 'test)
+
+  (check-equal? (interpret `(physical-to-logical-mapping bitwise ,(list (bv #b1 1) (bv #b0 1))))
+                (list (bv #b01 2)))
 
   (check-equal? (interpret `(logical-to-physical-inputs ,(lambda (x) x) ,(list (bv 1 1) (bv 0 1))))
                 (list (bv 1 1) (bv 0 1))))
