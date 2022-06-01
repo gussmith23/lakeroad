6e05ae716c3f4f142648fb8c43c6cbc50ae6d873
Gus Smith <guscomps@gmail.com>
2022-06-01 12:49:43 -0700

    Tighten up matching in interpreter

--- racket/interpreter.rkt
+++ racket/interpreter.rkt
@@ -4,26 +4,20 @@
 (provide interpret)
 
 (require "logical-to-physical.rkt"
-         "ultrascale.rkt")
+         "ultrascale.rkt"
+         rosette)
 
 (define (interpret expr)
   (match expr
     [`(logical-to-physical-mapping ,_ ...) (interpret-logical-to-physical-mapping interpret expr)]
     [`(physical-to-logical-mapping ,_ ...) (interpret-physical-to-logical-mapping interpret expr)]
     [`(ultrascale-plus-clb ,_ ...) (interpret-ultrascale-plus interpret expr)]
-    ;;; Everything else gets returned as-is. This means that calling (interpret) on Racket expressions
-    ;;; gives back Racket expressions, meaning we can use Racket expressions in our DSL.
-    ;;; It would be better if we could have a way that would catch invalid syntax...
-    [other other]))
+    [(? list? v) (map interpret v)]
+    [(? bv? v) v]))
 
 (module+ test
   (require rackunit
            rosette)
-
-  ;;; Interpreting Racket expressions should give back Racket expressions.
-  (check-equal? (interpret 1) 1)
-  (check-equal? (interpret (list 1 2 3)) (list 1 2 3))
-  (check-equal? (interpret 'test) 'test)
 
   (check-equal? (interpret `(physical-to-logical-mapping (bitwise) ,(list (bv #b1 1) (bv #b0 1))))
                 (list (bv #b01 2)))
