0dde103e6729761595f89ed6cf5b446ea55283ea
Gus Smith <guscomps@gmail.com>
2022-07-06 00:52:30 -0700

    Fix bug

--- racket/utils.rkt
+++ racket/utils.rkt
@@ -40,31 +40,31 @@
 ;;; Rosette bitvector expression to C expression.
 (define (bvexpr->cexpr expr)
   (match expr
-    [(expression @bvult a b)
+    [(expression (== bvult) a b)
      (when (> (bvlen a) 64)
        (error))
      (when (> (bvlen b) 64)
        (error))
      (format "((uint8_t)((uint64_t)~a < (uint64_t)~a))" (bvexpr->cexpr a) (bvexpr->cexpr b))]
-    [(expression @bvule a b)
+    [(expression (== bvule) a b)
      (when (> (bvlen a) 64)
        (error))
      (when (> (bvlen b) 64)
        (error))
      (format "((uint8_t)((uint64_t)~a <= (uint64_t)~a))" (bvexpr->cexpr a) (bvexpr->cexpr b))]
-    [(expression @bvugt a b)
+    [(expression (== bvugt) a b)
      (when (> (bvlen a) 64)
        (error))
      (when (> (bvlen b) 64)
        (error))
      (format "((uint8_t)((uint64_t)~a > (uint64_t)~a))" (bvexpr->cexpr a) (bvexpr->cexpr b))]
-    [(expression @bvuge a b)
+    [(expression (== bvuge) a b)
      (when (> (bvlen a) 64)
        (error))
      (when (> (bvlen b) 64)
        (error))
      (format "((uint8_t)((uint64_t)~a >= (uint64_t)~a))" (bvexpr->cexpr a) (bvexpr->cexpr b))]
-    [(expression @ite cond a b)
+    [(expression (== ite) cond a b)
      (format "(~a ? ~a : ~a)" (bvexpr->cexpr cond) (bvexpr->cexpr a) (bvexpr->cexpr b))]
     [(expression (== bveq) a b) (format "((uint8_t)(~a == ~a))" (bvexpr->cexpr a) (bvexpr->cexpr b))]
     [(expression (== bvand) a b) (format "(~a & ~a)" (bvexpr->cexpr a) (bvexpr->cexpr b))]
