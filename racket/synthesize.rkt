e4ec82ff87eb5c065b2e9751eaa441eb8d1461f7
David Cao <david@cao.sh>
2022-07-13 15:38:42 -0700

    progress

--- racket/synthesize.rkt
+++ racket/synthesize.rkt
@@ -135,19 +135,9 @@
   ;;; Zero-extend them so they're all the same size.
   ;;; TODO I think there's an error here --- Don't we need to pad to length 6?
   ; (define logical-inputs
-  ;   (map (lambda (v)
-  ;          (if (not (= (bvlen v) max-input-bw)) (dup-extend v (bitvector max-input-bw)) v))
-  ;        (symbolics bv-expr)))
-  ; (define logical-inputs
-  ;   (map (lambda (v)
-  ;          (choose `(zero-extend ,v ,(bitvector max-input-bw))
-  ;                  `(dup-extend this-is-a-hack-for-dup-extend ,v ,(bitvector max-input-bw))))
-  ;        (symbolics bv-expr)))
+  ;   (map (lambda (v) (zero-extend v (bitvector max-input-bw))) (symbolics bv-expr)))
   (define logical-inputs
-    (map (lambda (v)
-           (choose (zero-extend v (bitvector max-input-bw))
-                   (if (not (= (bvlen v) max-input-bw)) (dup-extend v (bitvector max-input-bw)) v)))
-         (symbolics bv-expr)))
+    (map (lambda (v) (choose `(zero-extend ,v ,(bitvector max-input-bw)) `(dup-extend ,v ,(bitvector max-input-bw)))) (symbolics bv-expr)))
 
   (define lut-fn
     (match (length (symbolics bv-expr))
