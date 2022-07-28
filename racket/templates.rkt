d6ca75e5bd0ff3e543fa268b94b48ea9b8601136
Gus Smith <guscomps@gmail.com>
2022-07-28 14:12:51 -0700

    Add template for lut with carry

--- racket/templates.rkt
+++ racket/templates.rkt
@@ -4,6 +4,7 @@
 
 (require rosette
          rosette/lib/angelic
+         rosette/lib/synthax
          "utils.rkt"
          "interpreter.rkt")
 
@@ -15,6 +16,9 @@
 ;;; - Inputs being mapped into luts (and outputs mapped out of luts) via bitwise or bitwise-reverse
 ;;;   patterns.
 ;;; - Inputs being zero-extended or duplicate-extended (in which the LSB is replicated to all bits).
+;;;
+;;; nbits is the bitwidth of the computation. Should likely be set to the max of the bitwidth of the
+;;; output and all of the inputs. Most likely, the output and all inputs will have the same bitwidth.
 (define (lut nbits architecture logical-inputs lutmems outwidth)
   (let* (;;; Max bitwidth, between all inputs and the output.
          [max-bw (apply max outwidth (map bvlen logical-inputs))]
@@ -47,6 +51,26 @@
 
     lakeroad-expr))
 
+(define (lut-with-carry nbits architecture logical-inputs lutmems outwidth)
+  (let* (;;; First: generate the simple LUT template.
+         [lakeroad-expr-lut (lut nbits architecture logical-inputs lutmems outwidth)]
+
+         ;;; Then, we add on top of it.
+         [max-bw (apply max outwidth (map bvlen logical-inputs))]
+         [logical-inputs
+          (map (lambda (v)
+                 (choose* `(zero-extend ,v ,(bitvector max-bw))
+                          `(dup-extend this-is-a-hack-for-dup-extend ,v ,(bitvector max-bw))))
+               logical-inputs)]
+         [logical-input (match logical-inputs
+                          [(list l0) (choose* l0)]
+                          [(list l0 l1) (choose* l0 l1)])]
+
+         [lakeroad-expr-carry
+          `(carry nbits architecture ,(?? (bitvector 1)) ,logical-input ,lakeroad-expr-lut)]
+         [output (choose* (lr-first lakeroad-expr-carry) (lr-second lakeroad-expr-carry))])
+    output))
+
 (module+ test
   (require rackunit
            "interpreter.rkt"
@@ -70,7 +94,28 @@
 
     (sat? soln))
 
+  (define (test-lut-with-carry-template bvexpr #:num-lutmems [num-lutmems 1])
+
+    ;;; Maximum number of input and output bitwidths = the number of bits we need to support.
+    (define nbits (apply max (bvlen bvexpr) (map bvlen (symbolics bvexpr))))
+    (define lutmems
+      (for/list ([i num-lutmems])
+        (define-symbolic* lutmem (bitvector (expt 2 (length (symbolics bvexpr)))))
+        lutmem))
+
+    (define lakeroad-expr (lut-with-carry nbits 'generic (symbolics bvexpr) lutmems (bvlen bvexpr)))
+
+    (define soln
+      (synthesize #:forall (symbolics bvexpr)
+                  #:guarantee (assert (bveq (interpret lakeroad-expr) bvexpr))))
+
+    (sat? soln))
+
   (define-symbolic a b (bitvector 8))
+  (define-symbolic s (bitvector 1))
 
   (test-true "8-bit identity" (test-lut-template a))
-  (test-true "8-bit and" (test-lut-template (bvand a b))))
+  (test-true "8-bit and" (test-lut-template (bvand a b)))
+  (test-true "8-bit mux" (test-lut-template (if (bvzero? s) a b)))
+  (test-true "8-bit add" (test-lut-with-carry-template (bvadd a b)))
+  (test-true "8-bit sub" (test-lut-with-carry-template (bvsub a b))))
