465e60ed41da0be5a20012f22e890f3f30d29c80
Gus Smith <guscomps@gmail.com>
2022-07-28 11:49:35 -0700

    Use generic hardware blocks in template

--- racket/templates.rkt
+++ racket/templates.rkt
@@ -15,7 +15,7 @@
 ;;; - Inputs being mapped into luts (and outputs mapped out of luts) via bitwise or bitwise-reverse
 ;;;   patterns.
 ;;; - Inputs being zero-extended or duplicate-extended (in which the LSB is replicated to all bits).
-(define (lut nbits lut-fn logical-inputs lutmems outwidth)
+(define (lut nbits architecture logical-inputs lutmems outwidth)
   (let* (;;; Max bitwidth, between all inputs and the output.
          [max-bw (apply max outwidth (map bvlen logical-inputs))]
          ;;; Form the list of logical inputs.
@@ -32,7 +32,7 @@
                                     ,(choose* '(bitwise) '(bitwise-reverse))
                                     ,logical-inputs)]
                  [physical-outputs (for/list ([i max-bw])
-                                     `(,lut-fn ,lutmem (list-ref ,physical-inputs ,i)))])
+                                     `(lut ,architecture ,lutmem (list-ref ,physical-inputs ,i)))])
             `(extract ,(sub1 outwidth)
                       0
                       (first (physical-to-logical-mapping ,(choose* '(bitwise) '(bitwise-reverse))
@@ -49,27 +49,24 @@
            rosette/solver/smt/boolector)
   (current-solver (boolector))
 
-  (define (test-lut-template bvexpr lut-fn lutmem-width #:num-lutmems [num-lutmems 1])
+  (define (test-lut-template bvexpr #:num-lutmems [num-lutmems 1])
 
     ;;; Maximum number of input and output bitwidths = the number of bits we need to support.
     (define nbits (apply max (bvlen bvexpr) (map bvlen (symbolics bvexpr))))
     (define lutmems
       (for/list ([i num-lutmems])
-        (define-symbolic* lutmem (bitvector lutmem-width))
+        (define-symbolic* lutmem (bitvector (expt 2 (length (symbolics bvexpr)))))
         lutmem))
 
-    (define lakeroad-expr (lut nbits lut-fn (symbolics bvexpr) lutmems (bvlen bvexpr)))
+    (define lakeroad-expr (lut nbits 'generic (symbolics bvexpr) lutmems (bvlen bvexpr)))
 
     (define soln
       (synthesize #:forall (symbolics bvexpr)
                   #:guarantee (assert (bveq (interpret lakeroad-expr) bvexpr))))
-    (when (sat? soln)
-      (pretty-display (evaluate lakeroad-expr soln)))
 
     (sat? soln))
 
   (define-symbolic a b (bitvector 8))
 
-  (test-true "8-bit UltraScale+ identity" (test-lut-template a 'ultrascale-plus-lut2 4))
-  (test-true "8-bit UltraScale+ and" (test-lut-template (bvand a b) 'ultrascale-plus-lut2 4))
-  (test-true "8-bit Lattice and" (test-lut-template (bvand a b) 'sofa-lut2 4)))
+  (test-true "8-bit identity" (test-lut-template a))
+  (test-true "8-bit and" (test-lut-template (bvand a b))))
