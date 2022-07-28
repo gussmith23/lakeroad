8affc324041fc687ff39aeb6f4d8e7b190eeb567
Gus Smith <guscomps@gmail.com>
2022-07-28 11:00:51 -0700

    Basic template

--- racket/templates.rkt
+++ racket/templates.rkt
@@ -3,50 +3,45 @@
 (provide lut)
 
 (require rosette
-         rosette/lib/synthax
          rosette/lib/angelic
          "utils.rkt"
-         "interpreter.rkt"
-         (prefix-in lr: "language.rkt"))
+         "interpreter.rkt")
 
-(define-grammar
- (lut-grammar nbits lut-fn logical-inputs lutmems outwidth)
- [out (lr:extract (lr:integer (sub1 outwidth)) (lr:integer 0) (logical-value))]
- [logical-value
-  (lr:bv (choose (bv #xff nbits)
-                 (bv #x00 nbits)
-                 ;`(first ,logical-inputs)
-                 ;;; these cause breakage
-                 (lr:list-ref (lr:integer 0) (logical-list))
-                 (lr:list-ref (lr:integer 1) (logical-list))
-                 (lr:list-ref (lr:integer 2) (logical-list))))]
- [logical-list
-  (lr:list (choose (map lr:bv logical-inputs)
-                   (lr:legacy `(physical-to-logical-mapping
-                                ,(choose '(bitwise) `(choose-one ,(bv 0 1)) '(bitwise-reverse))
-                                ,(for/list ([i nbits])
-                                   `(,lut-fn ,(lutmem)
-                                             (list-ref (logical-to-physical-mapping
-                                                        ,(choose '(bitwise) '(bitwise-reverse))
-                                                        ,(logical-list))
-                                                       ,i)))))))]
- [lutmem
-  (lr:bv (match lutmems
-           [(list i0) (choose i0)]
-           [(list i0 i1) (choose i0 i1)]
-           [(list i0 i1 i2) (choose i0 i1 i2)]))]
- [physical-list
-  (let* ([logical-to-physical
-          `(logical-to-physical-mapping ,(choose '(bitwise) '(bitwise-reverse)) ,(logical-list))]
-         [lut-output (for/list ([i nbits])
-                       `(,lut-fn ,(lutmem) (list-ref ,(physical-list) ,i)))])
-    ; (choose logical-to-physical lut-output)
-    lut-output)])
+;;; Simple LUT template which supports
+;;; - Arbitrary number of inputs.
+;;; - Inputs of arbitrary bitwidths.
+;;; - A single output of arbitrary bitwidth.
+;;; - Inputs being fed into a single column of LUTs
+;;; - Inputs being mapped into luts (and outputs mapped out of luts) via bitwise or bitwise-reverse
+;;;   patterns.
+;;; - Inputs being zero-extended or duplicate-extended (in which the LSB is replicated to all bits).
+(define (lut nbits lut-fn logical-inputs lutmems outwidth)
+  (let* (;;; Max bitwidth, between all inputs and the output.
+         [max-bw (apply max outwidth (map bvlen logical-inputs))]
+         ;;; Form the list of logical inputs.
+         ;;; Zero-extend them so they're all the same size.
+         [logical-inputs
+          (map (lambda (v)
+                 (choose* `(zero-extend ,v ,(bitvector max-bw))
+                          `(dup-extend this-is-a-hack-for-dup-extend ,v ,(bitvector max-bw))))
+               logical-inputs)]
+         [lutmem (match lutmems
+                   [(list lm0) (choose* lm0)])]
+         [lakeroad-expr
+          (let* ([physical-inputs `(logical-to-physical-mapping
+                                    ,(choose* '(bitwise) '(bitwise-reverse))
+                                    ,logical-inputs)]
+                 [physical-outputs (for/list ([i max-bw])
+                                     `(,lut-fn ,lutmem (list-ref ,physical-inputs ,i)))])
+            `(extract ,(sub1 outwidth)
+                      0
+                      (first (physical-to-logical-mapping ,(choose* '(bitwise) '(bitwise-reverse))
+                                                          ,physical-outputs))))])
 
-(define (lut nbits lut-fn logical-inputs lutmems outwidth #:depth depth)
-  ;;; First, extend the inputs up to the same size.
-  (let* ([logical-inputs (map (λ (i) (zero-extend i (bitvector nbits))) logical-inputs)])
-    (lut-grammar nbits lut-fn logical-inputs lutmems outwidth #:depth depth)))
+    (when (not (concrete? max-bw))
+      (error "Input bitwidths must be statically known."))
+
+    lakeroad-expr))
 
 (module+ test
   (require rackunit
@@ -54,19 +49,20 @@
            rosette/solver/smt/boolector)
   (current-solver (boolector))
 
-  (define (test-lut-template bvexpr lut-fn lutmem-width #:depth depth #:num-lutmems [num-lutmems 1])
+  (define (test-lut-template bvexpr lut-fn lutmem-width #:num-lutmems [num-lutmems 1])
 
     ;;; Maximum number of input and output bitwidths = the number of bits we need to support.
     (define nbits (apply max (bvlen bvexpr) (map bvlen (symbolics bvexpr))))
     (define lutmems
       (for/list ([i num-lutmems])
-        (constant `(lutmem ,i) (bitvector lutmem-width))))
+        (define-symbolic* lutmem (bitvector lutmem-width))
+        lutmem))
 
-    (define lakeroad-expr (lut nbits lut-fn (symbolics bvexpr) lutmems (bvlen bvexpr) #:depth depth))
+    (define lakeroad-expr (lut nbits lut-fn (symbolics bvexpr) lutmems (bvlen bvexpr)))
 
     (define soln
       (synthesize #:forall (symbolics bvexpr)
-                  #:guarantee (assert (bveq (interpret-new lakeroad-expr) bvexpr))))
+                  #:guarantee (assert (bveq (interpret lakeroad-expr) bvexpr))))
     (when (sat? soln)
       (pretty-display (evaluate lakeroad-expr soln)))
 
@@ -74,8 +70,6 @@
 
   (define-symbolic a b (bitvector 8))
 
-  (test-true "8-bit UltraScale+ identity" (test-lut-template a 'ultrascale-plus-lut2 4 #:depth 2))
-
-  (test-true "8-bit UltraScale+ and"
-             (test-lut-template (bvand a b) 'ultrascale-plus-lut2 4 #:depth 3))
-  (test-true "8-bit Lattice and" (test-lut-template (bvand a b) 'sofa-lut2 4 #:depth 3)))
+  (test-true "8-bit UltraScale+ identity" (test-lut-template a 'ultrascale-plus-lut2 4))
+  (test-true "8-bit UltraScale+ and" (test-lut-template (bvand a b) 'ultrascale-plus-lut2 4))
+  (test-true "8-bit Lattice and" (test-lut-template (bvand a b) 'sofa-lut2 4)))
