dffe2de9d832fa022f3342ac56eda5a2a925bdb0
Gus Smith <guscomps@gmail.com>
2022-08-12 13:41:23 -0700

    Don't add a bunch of new constructs yet
    Wait til they're needed. I don't think we should replace the lowest-levl
    datatypes (list, int,etc ) with structs anyway, though Oak would
    probably say that we should wrap everything in a struct.

--- racket/language.rkt
+++ racket/language.rkt
@@ -1,18 +1,9 @@
 #lang racket/base
 ;;; Base Lakeroad language.
 
-(provide list-ref extract bv integer list legacy)
+(provide list-ref
+         extract)
 
 ;;; Functions from Racket/Rosette.
 (struct list-ref (lst pos) #:transparent)
 (struct extract (h l v) #:transparent)
-
-;;; Base values.
-;;;
-;;; Rosette bitvector.
-(struct bv (v) #:transparent)
-(struct integer (v) #:transparent)
-(struct list (l) #:transparent)
-
-;;; Represents a legacy Lakeroad expression, expressed as lists of symbols/values.
-(struct legacy (expr) #:transparent)
