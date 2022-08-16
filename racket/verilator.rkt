d1bfce703479bd3ccdedff9422d4dc4142c63dc0
Gus Smith <guscomps@gmail.com>
2022-08-16 15:10:03 -0700

    WIP first pass, about to refactor to call Verilator a number of times

--- racket/verilator.rkt
+++ racket/verilator.rkt
@@ -8,6 +8,215 @@
          "utils.rkt"
          "interpreter.rkt")
 
+;;; Lakeroad expression to test.
+;;;
+;;; Args:
+;;;   lakeroad-expr: the Lakeroad expression to be compiled to Verilog and tested.
+;;;   bv-expr: the ground truth expression to be tested against.
+(struct to-simulate (lakeroad-expr bv-expr))
+
+;;; Simulate Lakeroad expressions with Verilator.
+;;;
+;;; It's preferable to batch tests into a single Verilator testbench as possible, because Verilator
+;;; compilation takes much longer than synthesis with Lakeroad or testbench runtime.
+(define/contract
+ (simulate-with-verilator to-simulate-values
+                          #:additional-files-to-build [additional-files-to-build '()]
+                          #:include-dirs [include-dirs '()]
+                          #:extra-verilator-args [extra-verilator-args ""])
+ (->* ((listof to-simulate?))
+      (#:additional-files-to-build (listof string?)
+       #:include-dirs (listof string?)
+       #:extra-verilator-args string?)
+      boolean?)
+ (begin
+   (define/contract
+    (generate-code-for-lakeroad-expression to-simulate-value)
+    (-> to-simulate? (list/c string? string? path?))
+    (begin
+      (match-define (to-simulate lakeroad-expr bv-expr) to-simulate-value)
+      (define json-file (make-temporary-file "rkttmp~a.json"))
+      (define verilog-file (make-temporary-file "rkttmp~a.v"))
+      (define module-name
+        (path->string (path-replace-extension (file-name-from-path verilog-file) "")))
+      (display-to-file (jsexpr->string (lakeroad->jsexpr lakeroad-expr #:module-name module-name))
+                       json-file
+                       #:exists 'update)
+      (match-let
+       ([(list proc-stdout stdin proc-id stderr control-fn)
+         (process (format "yosys -p 'read_json ~a; write_verilog ~a'" json-file verilog-file))])
+       (control-fn 'wait)
+       (when (not (equal? 0 (control-fn 'exit-code)))
+         (error (format "Converting JSON to Verilog via Yosys failed:\nSTDOUT:\n~a\nSTDERR:\n~a"
+                        (port->string proc-stdout)
+                        (port->string stderr))))
+       (close-input-port proc-stdout)
+       (close-input-port stderr)
+       (close-output-port stdin))
+
+      ;;; The class name of the C++ class produced by Verilator after Verilating our .v file.
+      (define verilated-type-name (format "V~a" module-name))
+
+      ;;; The symbol for the variable which indicates which value of the given var we're checking
+      ;;; this iteration.
+      (define (idx-var var)
+        (format "i~a" var))
+
+      ;;; The symbol for the variable which holds the value that gets driven into the input of the
+      ;;; module.
+      (define (val-var var)
+        (format "v~a" var))
+
+      ;;; The sequence of `for (<idx-var> = 0; <idx-var> < <limit>; <idx-var>++)` statements which
+      ;;; open the loop for each input we're testing over.
+      (define for-loop-openings
+        (for/list ([var (symbolics bv-expr)])
+
+          ;;; The symbol for the variable which indicates which value of the given var we're checking
+          ;;; this iteration. This index variable will either be used directly as input, in the case
+          ;;; where we're doing exhaustive testing of all possible inputs, or won't be used at all, as
+          ;;; the input will be generated randomly.
+          (define idx-variable-name (idx-var var))
+
+          ;;; The max value that the index variable should run to (exclusive). We only do exhaustive
+          ;;; testing for up to 8 bits, so we max out at 256. Thus, for anything higher than 8 bits,
+          ;;; we will do random testing with 256 randomly-selected values of var. We can make this
+          ;;; more configurable if we want.
+          (define limit (max 256 (expt 2 (bvlen var))))
+
+          ;;; Open the for loop that iterates over this variable.
+          (format "for (long long int ~a = 0; ~a < ~a; ++~a) {"
+                  idx-variable-name
+                  idx-variable-name
+                  limit
+                  idx-variable-name)))
+
+      ;;; The sequence of variable assignments that set the module's input values in this loop
+      ;;; iteration. When a variable is 8 bits wide or less, then we test all of its possible values.
+      ;;; We do this by setting the input value to the index variable for this variable. The index
+      ;;; variable counts from 0 to the max value; e.g. to 1 for a 1 bit number, or 255 for an 8 bit
+      ;;; number. This gives us exhaustive testing on this input. In the case where the variable is
+      ;;; greater than 8 bits wide, we use randomly generated values for the input.
+      (define value-definitions
+        (for/list ([var (symbolics bv-expr)])
+          (format
+           "long long int ~a = ~a;"
+           (val-var var)
+           (if (> (bvlen var) 8)
+               "(((uint64_t)std::rand() & 0xff) << 56) | (((uint64_t)std::rand() & 0xff) << 48) | (((uint64_t)std::rand() & 0xff) << 40)| (((uint64_t)std::rand() & 0xff) << 32) | (std::rand() & 0xff << 24) | (std::rand() & 0xff << 16) | (std::rand() & 0xff << 8) | (std::rand() & 0xff)"
+               (idx-var var)))))
+
+      ;;; The sequence of assignments of the values to the module inputs.
+      (define module-input-assignments
+        (for/list ([var (symbolics bv-expr)])
+          (format "top->~a = ~a;" var (val-var var))))
+
+      ;;; Closing the for loops.
+      (define for-loop-closings (make-list (length (symbolics bv-expr)) "}"))
+
+      (define code
+        (format
+         #<<here-string-delimiter
+{
+  ~a *top = new ~a{contextp};
+  ~a
+  ~a
+  ~a
+  ~a
+  delete top;
+}
+here-string-delimiter
+         ;
+         verilated-type-name
+         verilated-type-name
+         (string-join for-loop-openings "\n")
+         (string-join value-definitions "\n")
+         (string-join module-input-assignments "\n")
+         (string-join for-loop-closings "\n")))
+
+      (list code verilated-type-name verilog-file)))
+
+   (define results
+     (for/list ([to-simulate-value to-simulate-values])
+       (generate-code-for-lakeroad-expression to-simulate-value)))
+   (define tests (string-join (map first results) "\n"))
+   (define verilated-type-names (map second results))
+   (define verilog-files (map third results))
+   (define verilator-make-dir (make-temporary-file "rkttmp~a" 'directory))
+   (define includes
+     (string-join
+      (for/list ([verilog-file verilog-files])
+        (format "#include \"~a\""
+                (build-path
+                 verilator-make-dir
+                 (format "V~a.h" (path-replace-extension (file-name-from-path verilog-file) "")))))
+      "\n"))
+
+   (define testbench-source
+     (format
+      #<<here-string-delimiter
+#include <cstdlib>
+~a
+
+int main(int argc, char **argv, char **env)
+{
+  std::srand(23);
+
+  VerilatedContext *contextp = new VerilatedContext;
+  contextp->commandArgs(argc, argv);
+
+  ~a
+
+  delete contextp;
+  return 0;
+}
+here-string-delimiter
+      ;
+      includes
+      tests))
+
+   (define testbench-file (make-temporary-file "rkttmp_testbench_~a.cc"))
+   (display-to-file testbench-source testbench-file #:exists 'update)
+
+   (define include-dirs-string
+     (string-join (for/list ([include-dir include-dirs])
+                    (format "-I~a" include-dir))))
+   (define executable-filepath (make-temporary-file))
+   (define verilator-command
+     (format
+      "verilator -o ~a --relative-includes --cc --build --exe -Wall -Wno-MULTITOP --Mdir ~a ~a ~a ~a ~a ~a"
+      executable-filepath
+      verilator-make-dir
+      (string-join (map path->string verilog-files) " ")
+      (string-join additional-files-to-build " ")
+      testbench-file
+      include-dirs-string
+      extra-verilator-args))
+
+   (match-let ([(list proc-stdout stdin proc-id stderr control-fn) (process verilator-command)])
+              ;;; Wait until Verilator completes.
+              (control-fn 'wait)
+              ;;; Error out if it errors.
+              (when (not (equal? 0 (control-fn 'exit-code)))
+                (error (format "Verilator command failed:\n~a\nSTDOUT:\n~a\nSTDERR:\n~a"
+                               verilator-command
+                               (port->string proc-stdout)
+                               (port->string stderr))))
+              (close-input-port proc-stdout)
+              (close-input-port stderr)
+              (close-output-port stdin))
+
+   (system* executable-filepath)))
+
+(module+ test
+
+  (require rosette
+           rackunit)
+
+  (test-case "Simple multi-design Verilator test"
+             (define-symbolic a b (bitvector 8))
+             (simulate-with-verilator (list (to-simulate a a) (to-simulate b b)))))
+
 ;;; Test a Lakeroad expression using a simple testbench.
 ;;;
 ;;; The testbench simply does an exhaustive check over all inputs.
