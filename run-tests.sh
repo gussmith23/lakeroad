d7fb34e4a75d2a8091d137e3c5305bf30be15751
Gus Smith <guscomps@gmail.com>
2022-08-19 13:13:55 -0700

    Don't run tests in parallel; let tests parallelize themselves

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,10 @@
 set -e
 
 cargo test --manifest-path rust/Cargo.toml -- --nocapture
-bash -c 'raco test -j `nproc` racket/*.rkt'
+# We could use the -j flag of raco test to run tests in parallel, but instead,
+# we expect every test to parallelize itself if needed. We get more value out of
+# parallelizing individual tests (like how we use make to parallelize Verilation
+# in end-to-end builds), and multiple levels of parallelization would probably
+# cause some thrashing.
+bash -c 'raco test racket/*.rkt'
 source utils/tests/run-tests.sh
