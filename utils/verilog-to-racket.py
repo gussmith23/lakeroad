59ef1ec5c642ba5da7f58e1b33667b0445fe8c32
Gus Smith <guscomps@gmail.com>
2022-08-23 14:39:33 -0700

    more WIP

--- utils/verilog-to-racket.py
+++ utils/verilog-to-racket.py
@@ -1,22 +1,48 @@
-"""Converts a Verilog module to a Racket (Rosette) function."""
-import argparse
+#!/usr/bin/env python3
+"""Convert a Verilog module to a Racket (Rosette) function."""
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--infile", nargs="?", type=argparse.FileType("r"), default=sys.stdin
-)
-parser.add_argument(
-    "--outfile", nargs="?", type=argparse.FileType("w"), default=sys.stdout
-)
-parser.add_argument(
-    "--define",
-    default=[],
-    action="append",
-    help="Variables to `define. Support may be buggy.",
-)
-parser.add_argument(
-    "--remove_string",
-    action=argparse.BooleanOptionalAction,
-    help="Remove `string` types. Seems to be necessary for Yosys.",
-)
-args = parser.parse_args()
+from pathlib import Path
+from typing import Union, List
+import subprocess
+
+
+StrOrPath = Union[str, Path]
+
+
+def verilog_to_racket(infiles: List[StrOrPath], top: str) -> str:
+    """Convert a Verilog module to a Racket (Rosette) function.
+
+    Args:
+        infiles: List of Verilog files needed to do conversion.
+        top: Name of the top module."""
+    subprocess.call(
+        args=[
+            "python3",
+            Path(__file__).parent / "convert-module-to-btor.py",
+            "--top",
+            top,
+        ]
+    )
+
+
+if __name__ == "__main__":
+    import argparse
+    import sys
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--infile",
+        nargs=1,
+        type=argparse.FileType("r"),
+        action="append",
+        required=True,
+    )
+    parser.add_argument(
+        "--outfile", nargs=1, type=argparse.FileType("w"), default=sys.stdout
+    )
+    parser.add_argument(
+        "--top", nargs=1, type=str, required=True, help="Top module name."
+    )
+    args = parser.parse_args()
+
+    verilog_to_racket(infiles=args.infile, top=args.top)
