9e5e9de77bda2f55c86f0633fcb45e8054fc8ad0
Gus Smith <guscomps@gmail.com>
2022-09-01 17:16:24 -0700

    Put symbolic defines in a block so that optional arg actually works

--- racket/testing.rkt
+++ racket/testing.rkt
@@ -11,18 +11,18 @@
 ;;; Verify lr-expr against bv-expr, and optionally add both to the list of expressions to simulate.
 ;;;
 ;;; When testing Lakeroad, we want everything to be both *verified* at the level of Rosette
-;;; expressions, but also *simulated* at the level of Verilator simulation. This helper macro does
-;;; the verification component, with its main contribution being that it keeps the Rosette symbolic
-;;; state clean by wrapping everything in `with-vc` and `with-terms`. It also optionally builds a
-;;; list of simulation tasks. Note: this macro does not actually run simulation. Simulation should
-;;; be run in a batch after verification using `simulate-with-verilator`. Simulating in a batch is
-;;; much faster than simulating each test individually, as we can parallelize Verilation/C++
-;;; compilation.
+;;; expressions, but also *simulated* at the level of Verilator simulation. This helper macro does the
+;;; verification component, with its main contribution being that it keeps the Rosette symbolic state
+;;; clean by wrapping everything in `with-vc` and `with-terms`. It also optionally builds a list of
+;;; simulation tasks. Note: this macro does not actually run simulation. Simulation should be run in a
+;;; batch after verification using `simulate-with-verilator`. Simulating in a batch is much faster
+;;; than simulating each test individually, as we can parallelize Verilation/C++ compilation.
 ;;;
 ;;; Args:
 ;;; - name: Name of the test. A string.
-;;; - symbolic-define-etc: (define-symbolic ...) statements, plus other statements needed to prepare
-;;;     the verification environment (e.g. any assert or assume statements you might need).
+;;; - symbolic-define-etc-block: (begin (define-symbolic ...)) statement (or whatever you want.)
+;;;     Whatever statements are needed to prepare the verification environment (could also contain e.g. any assert or
+;;;     assume statements you might need).
 ;;; - lr-expr: Lakeroad expression to verify.
 ;;; - bv-expr: Bitvector expression to verify against. We will assert that `(bveq lr-expr bv-expr)`.
 ;;; - add-f: Optional function which takes to-simulate structs. In the common case, this function
@@ -31,13 +31,13 @@
 (define-syntax verify-lakeroad-expression
   (syntax-rules ()
     ;;; Main macro implementation.
-    [(_ name symbolic-define-etc ... lr-expr bv-expr add-f)
+    [(_ name symbolic-define-etc-block lr-expr bv-expr add-f)
      (test-true
       (string-append name ": check that test state is normal")
       (normal?
        (let ([result (with-vc (with-terms
                                (begin
-                                 symbolic-define-etc ...
+                                 symbolic-define-etc-block
                                  (add-f (to-simulate lr-expr bv-expr))
                                  (test-true
                                   (string-append name ": verify lr-expr against bv-expr")
@@ -50,5 +50,5 @@
            (raise (result-value result)))
          result)))]
     ;;; Allows add-f to be optional.
-    [(_ name symbolic-define-etc ... lr-expr bv-expr)
-     (verify-and-add name symbolic-define-etc ... lr-expr bv-expr identity)]))
+    [(_ name symbolic-define-etc-block lr-expr bv-expr)
+     (verify-lakeroad-expression name symbolic-define-etc-block lr-expr bv-expr identity)]))
