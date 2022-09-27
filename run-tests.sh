758bc9135fdbb719117f0749043cb64e8e09ad1d
bkushigian <bkushigian@gmail.com>
2022-09-27 08:49:59 -0700

    Added llvm_config check in run-tests

--- run-tests.sh
+++ run-tests.sh
@@ -1,16 +1,27 @@
 set -e
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-LAKEROAD_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+LAKEROAD_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 declare -rx LAKEROAD_DIR
 
-pushd "$LAKEROAD_DIR" > /dev/null
+if [ -z ${LLVM_CONFIG+x}]; then
+    echo "LLVM_CONFIG is unset"
+    exit 1
+fi
 
-if [ -z ${VERILATOR_INCLUDE_DIR+x} ] ; then
-    verilator --version > /dev/null 2>&1  || { echo >&2 "VERILATOR_INCLUDE_DIR not set.  Aborting."; exit 1; }
+pushd "$LAKEROAD_DIR" >/dev/null
+
+if [ -z ${VERILATOR_INCLUDE_DIR+x} ]; then
+    verilator --version >/dev/null 2>&1 || {
+        echo >&2 "VERILATOR_INCLUDE_DIR not set.  Aborting."
+        exit 1
+    }
     VERILATOR_INCLUDE_DIR="$(realpath "$(dirname "$(which verilator)")"/../share/verilator/include)"
-    [ -d "$VERILATOR_INCLUDE_DIR" ] || { echo >&2 "VERILATOR_INCLUDE_DIR not a directory.  Aborting."; exit 1; }    
+    [ -d "$VERILATOR_INCLUDE_DIR" ] || {
+        echo >&2 "VERILATOR_INCLUDE_DIR not a directory.  Aborting."
+        exit 1
+    }
     declare -rx VERILATOR_INCLUDE_DIR
 fi
 
