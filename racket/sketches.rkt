a5c4e14f0c25c32501f1854064d40f7a466fb8cd
Gus Smith <guscomps@gmail.com>
2022-10-11 11:27:49 -0700

    Bitwise with carry sketch + test

--- racket/sketches.rkt
+++ racket/sketches.rkt
@@ -15,6 +15,7 @@
          (prefix-in lr: "language.rkt")
          rosette
          rosette/lib/angelic
+         rosette/lib/synthax
          "verilator.rkt"
          "utils.rkt")
 
@@ -62,53 +63,123 @@
        [out-expr (lr:list-ref logical-outputs (lr:integer 0))])
     out-expr))
 
+;;; Bitwise with carry sketch generator.
+(define (bitwise-with-carry-sketch-generator architecture-description
+                                             logical-inputs
+                                             num-logical-inputs
+                                             bitwidth)
+  (match-let*
+      (;;; Generate a bitwise sketch over the inputs. We use this to generate the S signal.
+       [bitwise-sketch (bitwise-sketch-generator architecture-description
+                                                 logical-inputs
+                                                 num-logical-inputs
+                                                 bitwidth)]
+
+       ;;; Pass the results into a carry. We populate the DI signal with one of the logical inputs.
+       [(list carry-expr _) (construct-interface
+                             architecture-description
+                             (interface-identifier "carry" (hash "width" bitwidth))
+                             (list (cons "CI" (lr:bv (?? (bitvector 1))))
+                                   (cons "DI" (lr:list-ref logical-inputs (lr:integer 0)))
+                                   (cons "S" bitwise-sketch)))]
+       [out-expr (lr:hash-ref carry-expr 'O)])
+    out-expr))
+
 (module+ test
   (require rackunit
            "interpreter.rkt"
-           "lattice-ecp5-lut4.rkt")
+           "lattice-ecp5-lut4.rkt"
+           "xilinx-ultrascale-plus-lut6.rkt"
+           "xilinx-ultrascale-plus-carry8.rkt")
 
   (test-case
    "bitwise sketch generator"
-   (begin
+   (with-terms
+    (begin
 
-     (define a (constant 'a (bitvector 2)))
-     (define b (constant 'b (bitvector 2)))
-     (define bv-expr (bvand a b))
-     (define sketch
-       (bitwise-sketch-generator (lattice-ecp5-architecture-description)
-                                 (lr:list (list (lr:bv a) (lr:bv b)))
-                                 2
-                                 2))
+      (define-symbolic a b (bitvector 2))
+      (define bv-expr (bvand a b))
+      (define sketch
+        (bitwise-sketch-generator (lattice-ecp5-architecture-description)
+                                  (lr:list (list (lr:bv a) (lr:bv b)))
+                                  2
+                                  2))
 
-     (define result
-       (with-vc
-        (with-terms
-         (synthesize
-          #:forall (list a b)
-          #:guarantee
-          (assert (bveq bv-expr
-                        (interpret
-                         sketch
-                         #:module-semantics
-                         (list (cons (cons "LUT4" "../f4pga-arch-defs/ecp5/primitives/slice/LUT4.v")
-                                     lattice-ecp5-lut4)))))))))
+      (define result
+        (with-vc
+         (with-terms
+          (synthesize
+           #:forall (list a b)
+           #:guarantee
+           (assert (bveq bv-expr
+                         (interpret
+                          sketch
+                          #:module-semantics
+                          (list (cons (cons "LUT4" "../f4pga-arch-defs/ecp5/primitives/slice/LUT4.v")
+                                      lattice-ecp5-lut4)))))))))
 
-     (check-true (normal? result))
-     (define soln (result-value result))
+      (check-true (normal? result))
+      (define soln (result-value result))
 
-     (define lr-expr
-       (evaluate
-        sketch
-        ;;; Complete the solution: fill in any symbolic values that *aren't* the logical inputs.
-        (complete-solution soln
-                           (set->list (set-subtract (list->set (symbolics sketch))
-                                                    (list->set (symbolics bv-expr)))))))
+      (define lr-expr
+        (evaluate
+         sketch
+         ;;; Complete the solution: fill in any symbolic values that *aren't* the logical inputs.
+         (complete-solution soln
+                            (set->list (set-subtract (list->set (symbolics sketch))
+                                                     (list->set (symbolics bv-expr)))))))
 
-     (when (not (getenv "VERILATOR_INCLUDE_DIR"))
-       (raise "VERILATOR_INCLUDE_DIR not set"))
-     (define include-dir
-       (build-path (get-lakeroad-directory) "f4pga-arch-defs/ecp5/primitives/slice"))
-     (check-true (simulate-with-verilator #:include-dirs (list include-dir)
-                                          #:extra-verilator-args "-Wno-UNUSED"
-                                          (list (to-simulate lr-expr (bvand a b)))
-                                          (getenv "VERILATOR_INCLUDE_DIR"))))))
+      (when (not (getenv "VERILATOR_INCLUDE_DIR"))
+        (raise "VERILATOR_INCLUDE_DIR not set"))
+      (define include-dir
+        (build-path (get-lakeroad-directory) "f4pga-arch-defs/ecp5/primitives/slice"))
+      (check-true (simulate-with-verilator #:include-dirs (list include-dir)
+                                           #:extra-verilator-args "-Wno-UNUSED"
+                                           (list (to-simulate lr-expr bv-expr))
+                                           (getenv "VERILATOR_INCLUDE_DIR"))))))
+
+  (test-case
+   "bitwise with carry sketch generator"
+   (with-terms
+    (begin
+
+      (define-symbolic a b (bitvector 8))
+      (define bv-expr (bvadd a b))
+      (define sketch
+        (bitwise-with-carry-sketch-generator (xilinx-ultrascale-plus-architecture-description)
+                                             (lr:list (list (lr:bv a) (lr:bv b)))
+                                             2
+                                             8))
+
+      (define result
+        (with-vc (with-terms
+                  (synthesize
+                   #:forall (list a b)
+                   #:guarantee
+                   (assert (bveq bv-expr
+                                 (interpret sketch
+                                            #:module-semantics
+                                            (list (cons (cons "LUT6" "../verilator_xilinx/LUT6.v")
+                                                        xilinx-ultrascale-plus-lut6)
+                                                  (cons (cons "CARRY8" "../verilator_xilinx/CARRY8.v")
+                                                        xilinx-ultrascale-plus-carry8)))))))))
+
+      (check-true (normal? result))
+      (define soln (result-value result))
+
+      (define lr-expr
+        (evaluate
+         sketch
+         ;;; Complete the solution: fill in any symbolic values that *aren't* the logical inputs.
+         (complete-solution soln
+                            (set->list (set-subtract (list->set (symbolics sketch))
+                                                     (list->set (symbolics bv-expr)))))))
+
+      (when (not (getenv "VERILATOR_INCLUDE_DIR"))
+        (raise "VERILATOR_INCLUDE_DIR not set"))
+      (define include-dir (build-path (get-lakeroad-directory) "verilator_xilinx"))
+      (check-true (simulate-with-verilator
+                   #:include-dirs (list include-dir)
+                   #:extra-verilator-args "-Wno-UNUSED -Wno-PINMISSING -Wno-WIDTH -Wno-TIMESCALEMOD"
+                   (list (to-simulate lr-expr bv-expr))
+                   (getenv "VERILATOR_INCLUDE_DIR")))))))
