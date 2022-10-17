9a1d8e5fdd0be62017b525554b68fba3083474e9
Gus Smith <guscomps@gmail.com>
2022-10-17 16:12:27 -0700

    modernize end to end tests

--- racket/ultrascale-plus-dsp48e2-end-to-end-tests.rkt
+++ racket/ultrascale-plus-dsp48e2-end-to-end-tests.rkt
@@ -11,24 +11,17 @@
            rackunit
            rosette)
 
-  (define includes-dir (build-path (get-lakeroad-directory) "verilator_xilinx"))
-  (define includes
-    (append (for/list ([mod (list "CARRY8.v" "LUT6_2.v" "LUT3.v" "LUT2.v" "LUT1.v")])
-              (format "~a/~a" includes-dir mod))
-            (list (build-path (get-lakeroad-directory) "verilator-unisims" "DSP48E2.v"))))
+  (define to-simulate-list (list))
   (define (end-to-end-test bv-expr)
-    (define with-vc-result (with-vc (with-terms (synthesize-xilinx-ultrascale-plus-dsp bv-expr))))
-    (when (failed? with-vc-result)
-      (raise (result-value with-vc-result)))
-    (define lakeroad-expr (result-value with-vc-result))
-    (check-not-equal? lakeroad-expr #f)
-
-    (simulate-expr
-     lakeroad-expr
-     bv-expr
-     #:includes includes
-     #:extra-verilator-args
-     "-Wno-LATCH -Wno-ASSIGNDLY --CFLAGS -DVL_TIME_STAMP64 --CFLAGS -DVL_NO_LEGACY -DXIL_XECLIB"))
+    (test-case
+     (format "~a-bit ~a" (bvlen bv-expr) bv-expr)
+     (begin
+       (define with-vc-result (with-vc (with-terms (synthesize-xilinx-ultrascale-plus-dsp bv-expr))))
+       (when (failed? with-vc-result)
+         (raise (result-value with-vc-result)))
+       (define lakeroad-expr (result-value with-vc-result))
+       (check-not-equal? lakeroad-expr #f)
+       (set! to-simulate-list (cons (to-simulate lakeroad-expr bv-expr) to-simulate-list)))))
 
   (for ([sz (list 1 2 3 4 5 6 7 8 16)])
     (after
@@ -38,21 +31,32 @@
      (check-equal? (bvlen l1) sz)
      (check-equal? (bvlen l2) sz)
      (check-equal? (bvlen l3) sz)
-     (test-true (format "~a-bit sub" sz) (end-to-end-test (bvsub l0 l1)))
-     (test-true (format "~a-bit mul-add-mul" sz)
-                (end-to-end-test (bvadd (bvmul (bvadd l0 l1) l2) l3)))
-     (test-true (format "~a-bit mul-add" sz) (end-to-end-test (bvadd (bvmul l0 l1) l2)))
-     (test-true (format "~a-bit add-mul" sz) (end-to-end-test (bvmul (bvadd l0 l1) l2)))
+     (end-to-end-test (bvsub l0 l1))
+     (end-to-end-test (bvadd (bvmul (bvadd l0 l1) l2) l3))
+     (end-to-end-test (bvadd (bvmul l0 l1) l2))
+     (end-to-end-test (bvmul (bvadd l0 l1) l2))
      ; Sub on pre-adder doesn't seem to be working.
      ;(test-true (format "~a-bit sub-mul" sz) (end-to-end-test (bvmul (bvsub l0 l1) l2)))
-     (test-true (format "~a-bit add squared" sz)
-                (end-to-end-test (bvmul (bvadd l0 l1) (bvadd l0 l1))))
+     (end-to-end-test (bvmul (bvadd l0 l1) (bvadd l0 l1)))
      ;(test-true (format "~a-bit sub squared" sz) (end-to-end-test (bvmul (bvsub l0 l1) (bvsub l0 l1))))
-     (test-true (format "~a-bit square" sz) (end-to-end-test (bvmul l0 l0)))
-     (test-true (format "~a-bit add" sz) (end-to-end-test (bvadd l0 l1)))
-     (test-true (format "~a-bit mul" sz) (end-to-end-test (bvmul l0 l1)))
-     (test-true (format "~a-bit identity" sz) (end-to-end-test l0))
+     (end-to-end-test (bvmul l0 l0))
+     (end-to-end-test (bvadd l0 l1))
+     (end-to-end-test (bvmul l0 l1))
+     (end-to-end-test l0)
      (begin
        (clear-vc!)
        (clear-terms!)
-       (collect-garbage)))))
+       (collect-garbage))))
+
+  (when (not (getenv "VERILATOR_INCLUDE_DIR"))
+    (raise "VERILATOR_INCLUDE_DIR not set"))
+  (define include-dir (build-path (get-lakeroad-directory) "verilator_xilinx"))
+  (test-true
+   "simulate all synthesized designs with Verilator"
+   (simulate-with-verilator
+    #:include-dirs (list (build-path (get-lakeroad-directory) "verilator_xilinx")
+                         (build-path (get-lakeroad-directory) "verilator-unisims"))
+    #:extra-verilator-args
+    "-Wno-TIMESCALEMOD -Wno-PINMISSING -Wno-UNUSED -Wno-LATCH -Wno-ASSIGNDLY --CFLAGS -DVL_TIME_STAMP64 --CFLAGS -DVL_NO_LEGACY -DXIL_XECLIB"
+    to-simulate-list
+    (getenv "VERILATOR_INCLUDE_DIR"))))
