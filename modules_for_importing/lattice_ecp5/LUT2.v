3cc4138b77077908c3983744e6ee8bc732ba3fad
Gus Smith <guscomps@gmail.com>
2022-11-09 09:49:20 -0800

    More fixes

--- modules_for_importing/lattice_ecp5/LUT2.v
+++ modules_for_importing/lattice_ecp5/LUT2.v
@@ -1,9 +1,9 @@
 // Originally from https://github.com/uwsampl/f4pga-arch-defs/blob/d65a0ac5af639626d752dfccf58abea253dfba15/ecp5/primitives/slice/LUT2.v
 // 
 // Notes on conversion:
-// - Converted INIT from parameter to port.
+// - Converted init from parameter to port.
 `default_nettype none
-module LUT2(input A, B, output Z, input [3:0] INIT);
-    wire [1:0] s1 = B ?     INIT[ 3:2] :     INIT[1:0];
+module LUT2(input A, B, output Z, input [3:0] init);
+    wire [1:0] s1 = B ?     init[ 3:2] :     init[1:0];
     assign Z =      A ?          s1[1] :         s1[0];
 endmodule
