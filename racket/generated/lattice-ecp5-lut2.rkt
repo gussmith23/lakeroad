07c1bedcfe07deda7073743de7272273da0ab470
Gus Smith <guscomps@gmail.com>
2023-03-20 09:44:12 -0700

    Reimport lattice lut2

--- racket/generated/lattice-ecp5-lut2.rkt
+++ racket/generated/lattice-ecp5-lut2.rkt
@@ -8,21 +8,20 @@
 (provide lattice-ecp5-lut2)
 (require "../signal.rkt")
 (require rosette)
-(require racket/hash)
 (define lattice-ecp5-lut2
-  (λ (#:A (A (bv->signal (constant (list 'A 'symbolic-constant) (bitvector 1))))
-      #:B (B (bv->signal (constant (list 'B 'symbolic-constant) (bitvector 1))))
-      #:init (init (bv->signal (constant (list 'init 'symbolic-constant) (bitvector 4)))))
-    (let* ([merged-input-state-hash (hash)]
-           [init-hash (hash)]
+  (λ (#:A (A (bv->signal (constant 'A (bitvector 1))))
+      #:B (B (bv->signal (constant 'B (bitvector 1))))
+      #:init (init (bv->signal (constant 'init (bitvector 4)))))
+    (let* ([merged-input-state-hash (list)]
+           [init-hash (list)]
            [btor1 (bitvector 1)]
            [btor2 A]
-           [merged-input-state-hash (hash-union merged-input-state-hash (signal-state A))]
+           [merged-input-state-hash (append merged-input-state-hash (signal-state A))]
            [btor3 B]
-           [merged-input-state-hash (hash-union merged-input-state-hash (signal-state B))]
+           [merged-input-state-hash (append merged-input-state-hash (signal-state B))]
            [btor4 (bitvector 4)]
            [btor5 init]
-           [merged-input-state-hash (hash-union merged-input-state-hash (signal-state init))]
+           [merged-input-state-hash (append merged-input-state-hash (signal-state init))]
            [btor6 (bitvector 2)]
            [btor7 (signal (extract 1 0 (signal-value btor5)) (signal-state btor5))]
            [btor8 (signal (extract 3 2 (signal-value btor5)) (signal-state btor5))]
@@ -31,4 +30,4 @@
            [btor11 (signal (extract 1 1 (signal-value btor9)) (signal-state btor9))]
            [btor12 (if (bitvector->bool (signal-value btor2)) btor11 btor10)]
            [btor14 (bv->signal (zero-extend (signal-value btor9) (bitvector 2)) btor9)])
-      (make-immutable-hash (list (cons 'Z (signal (signal-value btor12) (hash))))))))
+      (list (cons 'Z (signal (signal-value btor12) (list)))))))
