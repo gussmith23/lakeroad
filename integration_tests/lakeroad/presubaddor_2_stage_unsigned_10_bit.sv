b3fec69d4b99ed272767fc7e0752bf6858d00851
Gus Smith <guscomps@gmail.com>
2023-05-25 10:41:42 -0700

    Error if LAKEROAD_PRIVATE_DIR not set

--- integration_tests/lakeroad/presubaddor_2_stage_unsigned_10_bit.sv
+++ integration_tests/lakeroad/presubaddor_2_stage_unsigned_10_bit.sv
@@ -14,26 +14,35 @@
 // RUN:  --input-signal c:10 \
 // RUN:  --input-signal d:10 > $outfile
 // RUN: FileCheck %s < $outfile
-// RUN: python $LAKEROAD_DIR/bin/simulate_with_verilator.py \
-// RUN:  --max_num_tests=10000 \
-// RUN:  --test_module_filepath $outfile \
-// RUN:  --ground_truth_module_filepath %s \
-// RUN:  --clock_name clk \
-// RUN:  --initiation_interval 2 \
-// RUN:  --output_signal_name out \
-// RUN:  --input_signal a:10 \
-// RUN:  --input_signal b:10 \
-// RUN:  --input_signal c:10 \
-// RUN:  --input_signal d:10 \
-// RUN:  --verilator_include_dir /home/gus/lakeroad-private/DSP48E2/ \
-// RUN:  --verilator_extra_arg='-DXIL_XECLIB' \
-// RUN:  --verilator_extra_arg='-Wno-UNOPTFLAT' \
-// RUN:  --verilator_extra_arg='-Wno-LATCH' \
-// RUN:  --verilator_extra_arg='-Wno-WIDTH' \
-// RUN:  --verilator_extra_arg='-Wno-STMTDLY' \
-// RUN:  --verilator_extra_arg='-Wno-CASEX' \
-// RUN:  --verilator_extra_arg='-Wno-TIMESCALEMOD' \
-// RUN:  --verilator_extra_arg='-Wno-PINMISSING'
+// Ideally, we would let people run these tests even without access to
+// lakeroad-private. Here, they can run the tests, and they'll still fail if
+// lakeroad-private isn't present, but at least they can see if they get past
+// synthesis.
+// RUN: if [ -z ${LAKEROAD_PRIVATE_DIR+x} ]; then \
+// RUN:   echo "Error: LAKEROAD_PRIVATE_DIR is not set. Skipping simulation."; \
+// RUN:   exit 1; \
+// RUN: else \
+// RUN:   python $LAKEROAD_DIR/bin/simulate_with_verilator.py \
+// RUN:    --max_num_tests=10000 \
+// RUN:    --test_module_filepath $outfile \
+// RUN:    --ground_truth_module_filepath %s \
+// RUN:    --clock_name clk \
+// RUN:    --initiation_interval 2 \
+// RUN:    --output_signal_name out \
+// RUN:    --input_signal a:10 \
+// RUN:    --input_signal b:10 \
+// RUN:    --input_signal c:10 \
+// RUN:    --input_signal d:10 \
+// RUN:    --verilator_include_dir "$LAKEROAD_PRIVATE_DIR/DSP48E2/" \
+// RUN:    --verilator_extra_arg='-DXIL_XECLIB' \
+// RUN:    --verilator_extra_arg='-Wno-UNOPTFLAT' \
+// RUN:    --verilator_extra_arg='-Wno-LATCH' \
+// RUN:    --verilator_extra_arg='-Wno-WIDTH' \
+// RUN:    --verilator_extra_arg='-Wno-STMTDLY' \
+// RUN:    --verilator_extra_arg='-Wno-CASEX' \
+// RUN:    --verilator_extra_arg='-Wno-TIMESCALEMOD' \
+// RUN:    --verilator_extra_arg='-Wno-PINMISSING'; \
+// RUN: fi
 
 module presubaddor_2_stage_unsigned_10_bit(
 	input  [9:0] a,
