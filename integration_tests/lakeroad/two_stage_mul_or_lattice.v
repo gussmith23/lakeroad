b540c7985f25228c5c1aa91372400c085fe7da80
Gus Smith <guscomps@gmail.com>
2023-07-25 16:12:17 -0700

    Whoops, fix test back to broken but correct state

--- integration_tests/lakeroad/two_stage_mul_or_lattice.v
+++ integration_tests/lakeroad/two_stage_mul_or_lattice.v
@@ -15,15 +15,14 @@
 
 module top(input clk, input [15:0] a, b, c, output [15:0] p);
 
-  reg [15:0] tmp0, tmp1, out;
+  reg [15:0] tmp0, tmp1;
 
   always @ (posedge clk) begin
     tmp0 <= (a * b) | c;
     tmp1 <= tmp0;
-    out <= tmp1;
   end
 
-  assign p = out;
+  assign p = tmp1;
 
 endmodule
 
