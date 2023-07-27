d96482ba0d0b5b44aa8e35b5cec1d66bea6b4fa3
Gus Smith <guscomps@gmail.com>
2023-07-27 13:38:37 -0700

    Update test

--- integration_tests/lakeroad/two_stage_multiplier_xilinx.v
+++ integration_tests/lakeroad/two_stage_multiplier_xilinx.v
@@ -1,4 +1,4 @@
-// RUN: racket $LAKEROAD_DIR/bin/main.rkt \
+// RUN: (racket $LAKEROAD_DIR/bin/main.rkt \
 // RUN:  --verilog-module-filepath %s \
 // RUN:  --architecture xilinx-ultrascale-plus \
 // RUN:  --template dsp \
@@ -10,6 +10,7 @@
 // RUN:  --module-name out \
 // RUN:  --input-signal a:16 \
 // RUN:  --input-signal b:16 \
+// RUN:  --timeout 120 || true) 2>&1 \
 // RUN: | FileCheck %s
 
 module two_stage_multiplier(input clk, input [15:0] a, b, output [15:0] p);
@@ -25,6 +26,6 @@ module two_stage_multiplier(input clk, input [15:0] a, b, output [15:0] p);
 
 endmodule
 
-// CHECK: module out(a, b, clk, p);
-// CHECK:   DSP48E2 #(
-// CHECK: endmodule
+// Check that the test times out.
+// CHECK: with-limit: out of time
+
