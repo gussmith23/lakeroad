789827d94ea150a706895807ab2074c5975a671e
Gus Smith <guscomps@gmail.com>
2023-07-28 18:05:11 -0700

    Update test

--- integration_tests/lakeroad/one_stage_add_mul_add_signed_18_bit_xilinx.sv
+++ integration_tests/lakeroad/one_stage_add_mul_add_signed_18_bit_xilinx.sv
@@ -1,3 +1,4 @@
+// RUN: outfile=$(mktemp)
 // RUN: racket $LAKEROAD_DIR/bin/main.rkt \
 // RUN:  --verilog-module-filepath %s \
 // RUN:  --architecture xilinx-ultrascale-plus \
@@ -7,12 +8,39 @@
 // RUN:  --verilog-module-out-signal out:18 \
 // RUN:  --initiation-interval 1 \
 // RUN:  --clock-name clk \
-// RUN:  --module-name out \
+// RUN:  --module-name top \
 // RUN:  --input-signal a:18 \
 // RUN:  --input-signal b:18 \
 // RUN:  --input-signal c:18 \
 // RUN:  --input-signal d:18 \
-// RUN: | FileCheck %s
+// RUN: > $outfile
+// RUN: cat $outfile
+// RUN: FileCheck %s < $outfile
+// RUN: if [ -z ${LAKEROAD_PRIVATE_DIR+x} ]; then \
+// RUN:   echo "Warning: LAKEROAD_PRIVATE_DIR is not set. Skipping simulation."; \
+// RUN:   exit 0; \
+// RUN: else \
+// RUN:   python $LAKEROAD_DIR/bin/simulate_with_verilator.py \
+// RUN:    --max_num_tests=10000 \
+// RUN:    --test_module_filepath $outfile \
+// RUN:    --ground_truth_module_filepath %s \
+// RUN:    --clock_name clk \
+// RUN:    --initiation_interval 1 \
+// RUN:    --output_signal_name out \
+// RUN:    --input_signal a:18 \
+// RUN:    --input_signal b:18 \
+// RUN:    --input_signal c:18 \
+// RUN:    --input_signal d:18 \
+// RUN:    --verilator_include_dir "$LAKEROAD_PRIVATE_DIR/DSP48E2/" \
+// RUN:    --verilator_extra_arg='-DXIL_XECLIB' \
+// RUN:    --verilator_extra_arg='-Wno-UNOPTFLAT' \
+// RUN:    --verilator_extra_arg='-Wno-LATCH' \
+// RUN:    --verilator_extra_arg='-Wno-WIDTH' \
+// RUN:    --verilator_extra_arg='-Wno-STMTDLY' \
+// RUN:    --verilator_extra_arg='-Wno-CASEX' \
+// RUN:    --verilator_extra_arg='-Wno-TIMESCALEMOD' \
+// RUN:    --verilator_extra_arg='-Wno-PINMISSING'; \
+// RUN: fi
 
 (* use_dsp = "yes" *) module top(
 	input signed [17:0] a,
@@ -31,3 +59,7 @@
 
 	assign out = stage0;
 endmodule
+
+// CHECK: module top(a, b, c, clk, d, out);
+// CHECK:   DSP48E2 #(
+// CHECK: endmodule
