4d7e57081199058850bb6e35e8cb7cc19e9e8aa1
Gus Smith <guscomps@gmail.com>
2023-07-29 20:35:04 -0700

    Test no longer times out

--- integration_tests/lakeroad/xilinx_addmulsub_3_stage_unsigned_18_bit.sv
+++ integration_tests/lakeroad/xilinx_addmulsub_3_stage_unsigned_18_bit.sv
@@ -1,5 +1,6 @@
-// RUN: (racket $LAKEROAD_DIR/bin/main.rkt \
-// RUN:  --solver cvc5 \
+// RUN: outfile=$(mktemp)
+// RUN: racket $LAKEROAD_DIR/bin/main.rkt \
+// RUN:  --solver bitwuzla \
 // RUN:  --verilog-module-filepath %s \
 // RUN:  --architecture xilinx-ultrascale-plus \
 // RUN:  --template dsp \
@@ -13,8 +14,34 @@
 // RUN:  --input-signal b:18 \
 // RUN:  --input-signal c:18 \
 // RUN:  --input-signal d:18 \
-// RUN:  --timeout 60 \
-// RUN:  || true) 2>&1 | FileCheck %s
+// RUN: > $outfile
+// RUN: FileCheck %s < $outfile
+// RUN: if [ -z ${LAKEROAD_PRIVATE_DIR+x} ]; then \
+// RUN:   echo "Warning: LAKEROAD_PRIVATE_DIR is not set. Skipping simulation."; \
+// RUN:   exit 0; \
+// RUN: else \
+// RUN:   python $LAKEROAD_DIR/bin/simulate_with_verilator.py \
+// RUN:    --max_num_tests=10000 \
+// RUN:    --test_module_filepath $outfile \
+// RUN:    --ground_truth_module_filepath %s \
+// RUN:    --clock_name clk \
+// RUN:    --initiation_interval 3 \
+// RUN:    --output_signal_name out \
+// RUN:    --input_signal a:18 \
+// RUN:    --input_signal b:18 \
+// RUN:    --input_signal c:18 \
+// RUN:    --input_signal d:18 \
+// RUN:    --verilator_include_dir "$LAKEROAD_PRIVATE_DIR/DSP48E2/" \
+// RUN:    --verilator_extra_arg='-DXIL_XECLIB' \
+// RUN:    --verilator_extra_arg='-Wno-UNOPTFLAT' \
+// RUN:    --verilator_extra_arg='-Wno-LATCH' \
+// RUN:    --verilator_extra_arg='-Wno-WIDTH' \
+// RUN:    --verilator_extra_arg='-Wno-STMTDLY' \
+// RUN:    --verilator_extra_arg='-Wno-CASEX' \
+// RUN: --testbench_stdout_log_filepath tmp.txt \
+// RUN:    --verilator_extra_arg='-Wno-TIMESCALEMOD' \
+// RUN:    --verilator_extra_arg='-Wno-PINMISSING'; \
+// RUN: fi
 
 (* use_dsp = "yes" *) module top(
 	input  [17:0] a,
@@ -37,4 +64,6 @@
 	assign out = stage2;
 endmodule
 
-// CHECK: Synthesis Timeout
+// CHECK: module top(a, b, c, clk, d, out);
+// CHECK:   DSP48E2 #(
+// CHECK: endmodule
