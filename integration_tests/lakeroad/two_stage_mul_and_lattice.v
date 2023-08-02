8d004f0eca5ac2fadd79a67306bf9077894cf111
Gus Smith <guscomps@gmail.com>
2023-08-01 21:07:39 -0700

    Test is now working

--- integration_tests/lakeroad/two_stage_mul_and_lattice.v
+++ integration_tests/lakeroad/two_stage_mul_and_lattice.v
@@ -1,4 +1,5 @@
-// RUN: (racket $LAKEROAD_DIR/bin/main.rkt \
+// RUN: outfile=$(mktemp)
+// RUN: racket $LAKEROAD_DIR/bin/main.rkt \
 // RUN:  --verilog-module-filepath %s \
 // RUN:  --architecture lattice-ecp5 \
 // RUN:  --template dsp \
@@ -7,13 +8,35 @@
 // RUN:  --verilog-module-out-signal p:16 \
 // RUN:  --initiation-interval 2 \
 // RUN:  --clock-name clk \
-// RUN:  --module-name out \
+// RUN:  --module-name top \
 // RUN:  --input-signal a:16 \
 // RUN:  --input-signal b:16 \
 // RUN:  --input-signal c:16 \
-// RUN: || true) \
-// RUN: 2>&1 \
-// RUN: | FileCheck %s
+// RUN:  > $outfile
+// RUN: FileCheck %s < $outfile
+// RUN: if [ -z ${LAKEROAD_PRIVATE_DIR+x} ]; then \
+// RUN:   echo "Warning: LAKEROAD_PRIVATE_DIR is not set. Skipping simulation."; \
+// RUN:   exit 0; \
+// RUN: else \
+// RUN:   python $LAKEROAD_DIR/bin/simulate_with_verilator.py \
+// RUN:    --max_num_tests=10000 \
+// RUN:    --test_module_filepath $outfile \
+// RUN:    --ground_truth_module_filepath %s \
+// RUN:    --clock_name clk \
+// RUN:    --initiation_interval 2 \
+// RUN:    --output_signal_name p \
+// RUN:    --input_signal a:16 \
+// RUN:    --input_signal b:16 \
+// RUN:    --input_signal c:16 \
+// RUN:    --verilator_include_dir "$LAKEROAD_PRIVATE_DIR/lattice_ecp5/" \
+// RUN:    --verilator_extra_arg='-Wno-CASEINCOMPLETE' \
+// RUN:    --verilator_extra_arg='-Wno-IMPLICIT' \
+// RUN:    --verilator_extra_arg='-Wno-PINMISSING' \
+// RUN:    --verilator_extra_arg='-Wno-TIMESCALEMOD' \
+// RUN:    --verilator_extra_arg='-Wno-UNOPTFLAT' \
+// RUN: --testbench_stdout_log_filepath tmp.txt \
+// RUN:    --verilator_extra_arg='-Wno-WIDTH'; \
+// RUN: fi
 
 module top(input clk, input [15:0] a, b, c, output [15:0] p);
 
@@ -28,4 +51,7 @@ module top(input clk, input [15:0] a, b, c, output [15:0] p);
 
 endmodule
 
-// CHECK: Synthesis failed
+// CHECK: module top(a, b, c, clk, p);
+// CHECK:   ALU54A #(
+// CHECK:   MULT18X18C #(
+// CHECK: endmodule
