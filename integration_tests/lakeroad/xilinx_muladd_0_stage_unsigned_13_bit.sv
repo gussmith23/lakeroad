9a3063430e78ebd5377cf96be7c74553ae47c374
Gus Smith <guscomps@gmail.com>
2023-08-09 22:03:11 -0700

    Add nonworking test

---
+++ integration_tests/lakeroad/xilinx_muladd_0_stage_unsigned_13_bit.sv
@@ -0,0 +1,55 @@
+// RUN: outfile=$(mktemp)
+// RUN: racket $LAKEROAD_DIR/bin/main.rkt \
+// RUN:  --solver bitwuzla \
+// RUN:  --verilog-module-filepath %s \
+// RUN:  --architecture xilinx-ultrascale-plus \
+// RUN:  --template dsp \
+// RUN:  --out-format verilog \
+// RUN:  --top-module-name top \
+// RUN:  --verilog-module-out-signal out:13 \
+// RUN:  --initiation-interval 0 \
+// RUN:  --module-name top \
+// RUN:  --input-signal a:13 \
+// RUN:  --input-signal b:13 \
+// RUN:  --input-signal c:13 \
+// RUN:  > $outfile
+// RUN: FileCheck %s < $outfile
+// RUN: if [ -z ${LAKEROAD_PRIVATE_DIR+x} ]; then \
+// RUN:   echo "Warning: LAKEROAD_PRIVATE_DIR is not set. Skipping simulation."; \
+// RUN:   exit 0; \
+// RUN: else \
+// RUN:   python $LAKEROAD_DIR/bin/simulate_with_verilator.py \
+// RUN:    --use_random_intermediate_inputs \
+// RUN:    --seed=23 \
+// RUN:    --max_num_tests=10000 \
+// RUN:    --test_module_filepath $outfile \
+// RUN:    --ground_truth_module_filepath %s \
+// RUN:    --initiation_interval 0 \
+// RUN:    --output_signal_name out \
+// RUN:    --input_signal a:13 \
+// RUN:    --input_signal b:13 \
+// RUN:    --input_signal c:13 \
+// RUN:    --verilator_include_dir "$LAKEROAD_PRIVATE_DIR/DSP48E2/" \
+// RUN:    --verilator_extra_arg='-DXIL_XECLIB' \
+// RUN:    --verilator_extra_arg='-Wno-UNOPTFLAT' \
+// RUN:    --verilator_extra_arg='-Wno-LATCH' \
+// RUN:    --verilator_extra_arg='-Wno-WIDTH' \
+// RUN:    --verilator_extra_arg='-Wno-STMTDLY' \
+// RUN:    --verilator_extra_arg='-Wno-CASEX' \
+// RUN:    --verilator_extra_arg='-Wno-TIMESCALEMOD' \
+// RUN:    --verilator_extra_arg='-Wno-PINMISSING'; \
+// RUN: fi
+
+(* use_dsp = "yes" *) module top(
+	input  [12:0] a,
+	input  [12:0] b,
+	input  [12:0] c,
+	output [12:0] out,
+	input clk);
+
+	assign out = (a * b) + c;
+endmodule
+
+// CHECK: module top(a, b, c, out);
+// CHECK:   DSP48E2 #(
+// CHECK: endmodule
