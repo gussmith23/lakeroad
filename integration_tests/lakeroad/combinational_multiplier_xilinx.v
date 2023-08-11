09c6c5265fcb29c492cbcedcb925a1382e1a8ecf
Gus Smith <guscomps@gmail.com>
2023-08-11 14:09:04 -0700

    Fix test

--- integration_tests/lakeroad/combinational_multiplier_xilinx.v
+++ integration_tests/lakeroad/combinational_multiplier_xilinx.v
@@ -7,13 +7,12 @@
 // RUN:  --top-module-name combinational_multiplier \
 // RUN:  --verilog-module-out-signal p:16 \
 // RUN:  --initiation-interval 0 \
-// RUN:  --clock-name clk \
 // RUN:  --module-name out \
 // RUN:  --input-signal a:16 \
 // RUN:  --input-signal b:16 \
 // RUN: | FileCheck %s
 
-module combinational_multiplier(input clk, input [15:0] a, b, output [15:0] p);
+module combinational_multiplier(input [15:0] a, b, output [15:0] p);
 
   assign p = a * b;
 
