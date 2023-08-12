901f261accda8d07024e2141d37f668e3d16d1bd
Gus Smith <guscomps@gmail.com>
2023-08-12 09:06:03 -0700

    Add timeout to tests that hang on remote

--- integration_tests/lakeroad/combinational_multiplier_xilinx.v
+++ integration_tests/lakeroad/combinational_multiplier_xilinx.v
@@ -11,6 +11,7 @@
 // RUN:  --module-name out \
 // RUN:  --input-signal a:16 \
 // RUN:  --input-signal b:16 \
+// RUN:  --timeout 90 \
 // RUN: | FileCheck %s
 
 module combinational_multiplier(input clk, input [15:0] a, b, output [15:0] p);
