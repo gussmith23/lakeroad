710d116d3735e48177dcf4d54d9f3ccacde580f1
Gus Smith <guscomps@gmail.com>
2023-08-30 21:32:24 -0700

    Add test infra and test

---
+++ lakeroad-egglog/tests/egglog_tests.rs
@@ -0,0 +1,17 @@
+use std::path::Path;
+
+macro_rules! egglog_test {
+    ($name:ident, $path:literal) => {
+        #[test]
+        fn $name() {
+            egglog::EGraph::default()
+                .parse_and_run_program(
+                    &std::fs::read_to_string(Path::new(env!("CARGO_MANIFEST_DIR")).join($path))
+                        .unwrap(),
+                )
+                .unwrap();
+        }
+    };
+}
+
+egglog_test!(test_1, "tests/egglog_tests/construct_sequential_cycle.egg");
