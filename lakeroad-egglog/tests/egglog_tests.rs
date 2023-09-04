e47784d6cd9d30d82fefd81aa148eee1c3a0df60
Gus Smith <guscomps@gmail.com>
2023-09-04 15:01:16 -0700

    Add commented code for producing SVGs

--- lakeroad-egglog/tests/egglog_tests.rs
+++ lakeroad-egglog/tests/egglog_tests.rs
@@ -4,12 +4,17 @@ macro_rules! egglog_test {
     ($name:ident, $path:literal) => {
         #[test]
         fn $name() {
-            egglog::EGraph::default()
+            let mut egraph = egglog::EGraph::default();
+            egraph
                 .parse_and_run_program(
                     &std::fs::read_to_string(Path::new(env!("CARGO_MANIFEST_DIR")).join($path))
                         .unwrap(),
                 )
                 .unwrap();
+            // Uncomment to see SVGs.
+            // let serialized = egraph.serialize_for_graphviz();
+            // let svg_path = Path::new($path).with_extension("svg");
+            // serialized.to_svg_file(svg_path).unwrap()
         }
     };
 }
