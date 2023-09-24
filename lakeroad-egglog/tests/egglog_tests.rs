773d08fe1621ee0547fe6dc52ff09c09d018cbd4
Gus Smith <guscomps@gmail.com>
2023-09-24 13:47:16 -0700

    Upgrade tests

--- lakeroad-egglog/tests/egglog_tests.rs
+++ lakeroad-egglog/tests/egglog_tests.rs
@@ -1,24 +1,48 @@
+use egglog::ExtractReport::*;
 use std::path::Path;
 
 macro_rules! egglog_test {
     ($name:ident, $path:literal) => {
+        egglog_test!($name, $path, egraph, {});
+    };
+    ($name:ident, $path:literal, $egraph_ident:ident, $after:block) => {
         #[test]
         fn $name() {
-            let mut egraph = egglog::EGraph::default();
-            egraph
+            let mut $egraph_ident = egglog::EGraph::default();
+            $egraph_ident
                 .parse_and_run_program(
                     &std::fs::read_to_string(Path::new(env!("CARGO_MANIFEST_DIR")).join($path))
                         .unwrap(),
                 )
                 .unwrap();
-            // Uncomment to see SVGs.
-            // let serialized = egraph.serialize_for_graphviz();
+            // Uncomment to see SVGs, or add the following lines to your $after.
+            // let serialized = $egraph_ident.serialize_for_graphviz();
             // let svg_path = Path::new($path).with_extension("svg");
-            // serialized.to_svg_file(svg_path).unwrap()
+            // serialized.to_svg_file(svg_path).unwrap();
+
+            $after
         }
     };
 }
 
 egglog_test!(test_1, "tests/egglog_tests/construct_sequential_cycle.egg");
-egglog_test!(permuter, "tests/egglog_tests/permuter.egg");
+egglog_test!(permuter, "tests/egglog_tests/permuter.egg", egraph, {
+    let report = egraph
+        .get_extract_report()
+        .as_ref()
+        .expect("no extract report");
+    let cost = match report {
+        &Best { cost, .. } => cost,
+        _ => panic!("Expected [`Best`]"),
+    };
+    assert!(
+        cost < 100000000,
+        "Cost >= 100000000. Presumably we extracted an Op."
+    );
+    dbg!(cost);
+
+    let serialized = egraph.serialize_for_graphviz();
+    let svg_path = Path::new("tmp.svg");
+    serialized.to_svg_file(svg_path).unwrap();
+});
 egglog_test!(typing, "tests/egglog_tests/typing.egg");
