27a98e740558995f11f1a0bc2035fe7206492ee9
Gus Smith <guscomps@gmail.com>
2024-04-30 09:18:18 -0700

    bug

--- release_files/run-examples.sh
+++ release_files/run-examples.sh
@@ -5,5 +5,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Run all examples in ./examples
 for example in "$SCRIPT_DIR/examples"/*; do
   echo "Running example $example"
-  ./"$example"
+  "$example"
 done
