dd856db5e0825b7b40a403277584acd1309a8021
Vishal Canumalla <vishalc@cs.washington.edu>
2024-05-13 15:35:30 -0700

    addmuladd

---
+++ integration_tests/lakeroad/xilinx-virtex/virtex_addmuladd_1_stage_signed_11_bit.sv
@@ -0,0 +1,73 @@
+// RUN: outfile=$(mktemp)
+// RUN: (racket $LAKEROAD_DIR/bin/main.rkt \
+// RUN:  --solver bitwuzla \
+// RUN:  --verilog-module-filepath %s \
+// RUN:  --architecture xilinx-virtex \
+// RUN:  --template dsp \
+// RUN:  --out-format verilog \
+// RUN:  --top-module-name top \
+// RUN:  --verilog-module-out-signal out:11 \
+// RUN:  --initiation-interval 3 \
+// RUN:  --clock-name clk \
+// RUN:  --module-name top \
+// RUN:  --input-signal a:11 \
+// RUN:  --input-signal b:11 \
+// RUN:  --input-signal c:11 \
+// RUN:  --input-signal d:11 \
+// RUN:  --timeout 120 \
+// RUN:  --extra-cycles 3 \
+// RUN:  || true) \
+// RUN:  > $outfile \
+// RUN:  2>&1
+// RUN: FileCheck %s < $outfile
+//  if [ -z ${LAKEROAD_PRIVATE_DIR+x} ]; then \
+//    echo "Warning: LAKEROAD_PRIVATE_DIR is not set. Skipping simulation."; \
+//    exit 0; \
+//  else \
+//    python3 $LAKEROAD_DIR/bin/simulate_with_verilator.py \
+//     --use_random_intermediate_inputs \
+//     --seed=23 \
+//     --max_num_tests=10000 \
+//     --verilog_filepath $outfile \
+//     --verilog_filepath %s \
+//     --clock_name clk \
+//     --initiation_interval 1 \
+//     --output_signal_name out \
+//     --input_signal a:11 \
+//     --input_signal b:11 \
+//     --input_signal c:11 \
+//     --input_signal d:11 \
+//     --verilator_include_dir "$LAKEROAD_PRIVATE_DIR/DSP48E2/" \
+//     --verilator_extra_arg='-DXIL_XECLIB' \
+//     --verilator_extra_arg='-Wno-UNOPTFLAT' \
+//     --verilator_extra_arg='-Wno-LATCH' \
+//     --verilator_extra_arg='-Wno-WIDTH' \
+//     --verilator_extra_arg='-Wno-STMTDLY' \
+//     --verilator_extra_arg='-Wno-CASEX' \
+//     --verilator_extra_arg='-Wno-TIMESCALEMOD' \
+//     --verilator_extra_arg='-Wno-PINMISSING'; \
+//     --verilator_extra_arg='-Wno-COMBDLY' \
+//  	 --verilator_extra_arg='-Wno-INITIALDLY' \
+//  	 --verilator_extra_arg='-Wno-CASEINCOMPLETE'; \
+//  fi
+
+
+(* use_dsp = "yes" *) module top(
+	input signed [10:0] a,
+	input signed [10:0] b,
+	input signed [10:0] c,
+	input signed [10:0] d,
+	output [10:0] out,
+	input clk);
+
+	logic signed [21:0] stage0;
+
+	always @(posedge clk) begin
+	stage0 <= ((d + a) * b) + c;
+
+	end
+
+	assign out = stage0;
+endmodule
+
+// CHECK: Synthesis Timeout
