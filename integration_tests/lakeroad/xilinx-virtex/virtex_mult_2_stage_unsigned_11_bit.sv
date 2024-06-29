a2c149358b25818d17c918edc872225e2dc77ecc
Gus Smith <guscomps@gmail.com>
2024-06-29 16:57:55 -0700

    test works in CI

--- integration_tests/lakeroad/xilinx-virtex/virtex_mult_2_stage_unsigned_11_bit.sv
+++ integration_tests/lakeroad/xilinx-virtex/virtex_mult_2_stage_unsigned_11_bit.sv
@@ -18,34 +18,34 @@
 // RUN:  > $outfile \
 // RUN:  2>&1
 // RUN: FileCheck %s < $outfile
-// if [ -z ${LAKEROAD_PRIVATE_DIR+x} ]; then \
-//   echo "Warning: LAKEROAD_PRIVATE_DIR is not set. Skipping simulation."; \
-//   exit 0; \
-// else \
-//   python3 $LAKEROAD_DIR/bin/simulate_with_verilator.py \
-//    --test_module_name out \
-//    --ground_truth_module_name top \
-//    --max_num_tests=10000 \
-//    --verilog_filepath $outfile \
-//    --verilog_filepath %s \
-//    --clock_name clk \
-//    --pipeline_depth 2 \
-//    --output_signal out:11 \
-//    --input_signal a:11 \
-//    --input_signal b:11 \ 
-//    --verilator_include_dir "$LAKEROAD_PRIVATE_DIR/DSP48E1/" \
-//    --verilator_extra_arg='-DXIL_XECLIB' \
-//    --verilator_extra_arg='-Wno-UNOPTFLAT' \
-//    --verilator_extra_arg='-Wno-LATCH' \
-//    --verilator_extra_arg='-Wno-WIDTH' \
-//    --verilator_extra_arg='-Wno-STMTDLY' \
-//    --verilator_extra_arg='-Wno-CASEX' \
-//    --verilator_extra_arg='-Wno-TIMESCALEMOD' \
-//    --verilator_extra_arg='-Wno-PINMISSING' \
-//    --verilator_extra_arg='-Wno-COMBDLY' \
-// 	 --verilator_extra_arg='-Wno-INITIALDLY' \
-// 	 --verilator_extra_arg='-Wno-CASEINCOMPLETE'; \
-// fi
+// RUN: if [ -z ${LAKEROAD_PRIVATE_DIR+x} ]; then \
+// RUN:   echo "Warning: LAKEROAD_PRIVATE_DIR is not set. Skipping simulation."; \
+// RUN:   exit 0; \
+// RUN: else \
+// RUN:   python3 $LAKEROAD_DIR/bin/simulate_with_verilator.py \
+// RUN:    --test_module_name out \
+// RUN:    --ground_truth_module_name top \
+// RUN:    --max_num_tests=10000 \
+// RUN:    --verilog_filepath $outfile \
+// RUN:    --verilog_filepath %s \
+// RUN:    --clock_name clk \
+// RUN:    --pipeline_depth 2 \
+// RUN:    --output_signal out:11 \
+// RUN:    --input_signal a:11 \
+// RUN:    --input_signal b:11 \ 
+// RUN:    --verilator_include_dir "$LAKEROAD_PRIVATE_DIR/DSP48E1/" \
+// RUN:    --verilator_extra_arg='-DXIL_XECLIB' \
+// RUN:    --verilator_extra_arg='-Wno-UNOPTFLAT' \
+// RUN:    --verilator_extra_arg='-Wno-LATCH' \
+// RUN:    --verilator_extra_arg='-Wno-WIDTH' \
+// RUN:    --verilator_extra_arg='-Wno-STMTDLY' \
+// RUN:    --verilator_extra_arg='-Wno-CASEX' \
+// RUN:    --verilator_extra_arg='-Wno-TIMESCALEMOD' \
+// RUN:    --verilator_extra_arg='-Wno-PINMISSING' \
+// RUN:    --verilator_extra_arg='-Wno-COMBDLY' \
+// RUN: 	 --verilator_extra_arg='-Wno-INITIALDLY' \
+// RUN: 	 --verilator_extra_arg='-Wno-CASEINCOMPLETE'; \
+// RUN: fi
 
 (* use_dsp = "yes" *) module top(
     input [10:0] a,
@@ -65,4 +65,4 @@
 	assign out = stage1;
 endmodule
 
-// CHECK: Synthesis Timeout
+// CHECK: module out(a, b, clk, out);
