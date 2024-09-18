f8ae3967151fac7bde87c1ed6df8cccfeea6e7fc
cknizek <87683288+cknizek@users.noreply.github.com>
2024-09-18 15:45:32 -0700

    Bsg mul add unsigned dev (#454)
    New (separate) PR to add the BaseJump STL mul-add-unsigned file into
    Lakeroad

---
+++ integration_tests/lakeroad/bsg_mul_add_unsigned/bsg_mul_add_unsigned_13bit.sv
@@ -0,0 +1,92 @@
+// RUN: outfile=$(mktemp)
+// RUN: racket $LAKEROAD_DIR/bin/main.rkt \
+// RUN:  --solver bitwuzla \
+// RUN:  --verilog-module-filepath %s \
+// RUN:  --architecture xilinx-ultrascale-plus \
+// RUN:  --template dsp \
+// RUN:  --out-format verilog \
+// RUN:  --top-module-name top \
+// RUN:  --verilog-module-out-signal o:27 \
+// RUN:  --pipeline-depth 0 \
+// RUN:  --clock-name clk_i \
+// RUN:  --module-name test_module \
+// RUN:  --input-signal a_i:13 \
+// RUN:  --input-signal b_i:13 \
+// RUN:  --input-signal c_i:26 \
+// RUN:  --timeout 270 \
+// RUN: > $outfile
+// RUN: cat $outfile
+// RUN: FileCheck %s < $outfile
+// RUN: if [ -z ${LAKEROAD_PRIVATE_DIR+x} ]; then \
+// RUN:   echo "Warning: LAKEROAD_PRIVATE_DIR is not set. Skipping simulation."; \
+// RUN:   exit 0; \
+// RUN: else \
+// RUN:   python3 $LAKEROAD_DIR/bin/simulate_with_verilator.py \
+// RUN:    --test_module_name test_module \
+// RUN:    --ground_truth_module_name top \
+// RUN:    --max_num_tests=10000 \
+// RUN:    --verilog_filepath $outfile \
+// RUN:    --verilog_filepath %s \
+// RUN:    --clock_name clk_i \
+// RUN:    --pipeline_depth 0 \
+// RUN:    --output_signal o:27 \
+// RUN:    --input_signal a_i:13 \
+// RUN:    --input_signal b_i:13 \
+// RUN:    --input_signal c_i:26 \
+// RUN:    --verilator_include_dir "$LAKEROAD_PRIVATE_DIR/DSP48E2/" \
+// RUN:    --verilator_extra_arg='-DXIL_XECLIB' \
+// RUN:    --verilator_extra_arg='-Wno-UNOPTFLAT' \
+// RUN:    --verilator_extra_arg='-Wno-LATCH' \
+// RUN:    --verilator_extra_arg='-Wno-WIDTH' \
+// RUN:    --verilator_extra_arg='-Wno-STMTDLY' \
+// RUN:    --verilator_extra_arg='-Wno-CASEX' \
+// RUN:    --verilator_extra_arg='-Wno-TIMESCALEMOD' \
+// RUN:    --verilator_extra_arg='-Wno-PINMISSING'; \
+// RUN: fi
+
+
+`include "bsg_defines.sv"
+`include "bsg_dff_chain.sv"
+  
+(* use_dsp = "yes" *) module top #(
+    parameter  width_a_p = 13
+    ,parameter width_b_p = 13
+    ,parameter width_c_p = 26
+    ,parameter width_o_p = 27
+    ,parameter pipeline_p = 0
+  ) (
+     input [width_a_p-1 : 0] a_i
+    ,input [width_b_p-1 : 0] b_i
+    ,input [width_c_p-1 : 0] c_i
+    ,input clk_i
+    ,output [width_o_p-1 : 0] o
+    );
+
+    localparam pre_pipeline_lp = pipeline_p > 2 ? 1 : 0;
+    localparam post_pipeline_lp = pipeline_p > 2 ? pipeline_p -1 : pipeline_p; 
+
+    wire [width_a_p-1:0] a_r;
+    wire [width_b_p-1:0] b_r;
+    wire [width_c_p-1:0] c_r;
+
+    bsg_dff_chain #(width_a_p + width_b_p + width_c_p, pre_pipeline_lp)
+        pre_mul_add (
+            .clk_i(clk_i)
+            ,.data_i({a_i, b_i, c_i})
+            ,.data_o({a_r, b_r, c_r})
+        );
+
+    wire [width_o_p-1:0] o_r = a_r * b_r + c_r;
+
+    bsg_dff_chain #(width_o_p, post_pipeline_lp)
+        post_mul_add (
+            .clk_i(clk_i)
+            ,.data_i(o_r)
+            ,.data_o(o)
+        );
+endmodule
+
+// CHECK: module test_module(a_i, b_i, c_i, clk_i, o);
+// CHECK:   DSP48E2 #(
+// CHECK: endmodule
+
