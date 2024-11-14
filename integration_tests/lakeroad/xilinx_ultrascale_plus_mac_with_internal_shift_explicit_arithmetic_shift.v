585d48c9fedb4c646ea3132f4b61c2e9f658ac36
Gus Smith <guscomps@gmail.com>
2024-11-14 09:34:53 +0700

    Add test that shifts a 48-bit number w/ explicit arithmetic shifting

---
+++ integration_tests/lakeroad/xilinx_ultrascale_plus_mac_with_internal_shift_explicit_arithmetic_shift.v
@@ -0,0 +1,51 @@
+// RUN: outfile=$(mktemp)
+// RUN: racket $LAKEROAD_DIR/bin/main.rkt \
+// RUN:  --solver bitwuzla \
+// RUN:  --verilog-module-filepath %s \
+// RUN:  --architecture xilinx-ultrascale-plus \
+// RUN:  --template dsp \
+// RUN:  --out-format verilog \
+// RUN:  --top-module-name top \
+// RUN:  --verilog-module-out-signal out:48 \
+// RUN:  --pipeline-depth 0 \
+// RUN:  --module-name out \
+// RUN:  --input-signal 'c:(port c 48):48' \
+// RUN:  --timeout 90 \
+// RUN: > $outfile
+// RUN: FileCheck %s < $outfile
+// RUN: if [ -z ${LAKEROAD_PRIVATE_DIR+x} ]; then \
+// RUN:   echo "Warning: LAKEROAD_PRIVATE_DIR is not set. Skipping simulation."; \
+// RUN:   exit 0; \
+// RUN: else \
+// RUN:   python3 $LAKEROAD_DIR/bin/simulate_with_verilator.py \
+// RUN:    --test_module_name out \
+// RUN:    --ground_truth_module_name top \
+// RUN:    --max_num_tests=10000 \
+// RUN:    --verilog_filepath $outfile \
+// RUN:    --verilog_filepath %s \
+// RUN:    --pipeline_depth 0 \
+// RUN:    --output_signal out:48 \
+// RUN:    --input_signal c:48 \
+// RUN:    --verilator_include_dir "$LAKEROAD_PRIVATE_DIR/DSP48E2/" \
+// RUN:    --verilator_extra_arg='-DXIL_XECLIB' \
+// RUN:    --verilator_extra_arg='-Wno-UNOPTFLAT' \
+// RUN:    --verilator_extra_arg='-Wno-LATCH' \
+// RUN:    --verilator_extra_arg='-Wno-WIDTH' \
+// RUN:    --verilator_extra_arg='-Wno-STMTDLY' \
+// RUN:    --verilator_extra_arg='-Wno-CASEX' \
+// RUN:    --verilator_extra_arg='-Wno-TIMESCALEMOD' \
+// RUN:    --verilator_extra_arg='-Wno-PINMISSING'; \
+// RUN: fi
+
+// TODO(@gussmith23): here, I explicitly do a signed shift, which does
+// synthesize. I assumed >> did a signed shift when the arguments are signed,
+// but perhaps I'm wrong. Why does this matter? Because it means we need to be
+// careful about how we do our rewriting in the egraph. We can't necessarily
+// just rewrite to a >> in the egraph without fully understanding the semantics.
+// So the task here is to understand how we should be writing the shift 
+// in a partial-product multiply-splitting scenario.
+module top(input [47:0] c, output [47:0] out);
+  assign out = {{17{c[47]}}, c[47:17]};
+endmodule
+
+// CHECK: module out(c, out);
