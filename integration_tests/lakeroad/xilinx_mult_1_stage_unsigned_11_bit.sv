e812ec5aef1cd292232c589ddd3b60182a0d37e0
Gus Smith <guscomps@gmail.com>
2025-04-15 22:27:32 -0700

    Switch solvers one last time

--- integration_tests/lakeroad/xilinx_mult_1_stage_unsigned_11_bit.sv
+++ integration_tests/lakeroad/xilinx_mult_1_stage_unsigned_11_bit.sv
@@ -1,6 +1,6 @@
 // RUN: outfile=$(mktemp)
 // RUN: racket $LAKEROAD_DIR/bin/main.rkt \
-// RUN:  --solver yices \
+// RUN:  --solver stp \
 // RUN:  --verilog-module-filepath %s \
 // RUN:  --architecture xilinx-ultrascale-plus \
 // RUN:  --template dsp \
